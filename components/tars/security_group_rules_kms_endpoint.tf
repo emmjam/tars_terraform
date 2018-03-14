82eaeab1442b1e0d41a38b3db00d39ae928e7ad6
Mark Thompson <mark.thompson@bjss.com>
2018-03-14 17:33:15 +0000

    Added SG rule to kms sg from backend

--- components/tars/security_group_rules_kms_endpoint.tf
+++ components/tars/security_group_rules_kms_endpoint.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "kms_endpoint_ingress_tars_core_frontend" {
   security_group_id        = "${data.terraform_remote_state.base.kms_sg_id}"
   source_security_group_id = "${aws_security_group.tars-core-frontend.id}"
 }
+
+resource "aws_security_group_rule" "kms_endpoint_ingress_tars_core_backend" {
+  description              = "Allow use of KMS endpoint"
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "-1"
+  security_group_id        = "${data.terraform_remote_state.base.kms_sg_id}"
+  source_security_group_id = "${aws_security_group.tars-core-backend.id}"
+}
