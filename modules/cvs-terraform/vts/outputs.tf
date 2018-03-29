36d784721bf8d8d4a1987aad4e4d9141339039b9
Callum Massey <callum.massey@bjss.com>
2018-03-29 12:18:15 +0100

    Remove unused code

--- modules/cvs-terraform/vts/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}/"
-}
-
-output "r53_fqdn" {
-  value = "${aws_route53_record.main.fqdn}"
-}
--- modules/cvs-terraform/webui/aws_data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_route53_zone" "public" {
-  count        = "${var.public_domain_name != "" ? 1 : 0 }"
-  name         = "${var.public_domain_name}."
-  private_zone = false
-}
