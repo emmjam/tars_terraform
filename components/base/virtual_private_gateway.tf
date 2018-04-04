7efa7ec7282ed3cf6effa1bcd89dada36e724120
Mark Thompson <mark.thompson@bjss.com>
2018-04-04 14:01:11 +0100

    Added transit VPC peering vpg and route propagation

---
+++ components/base/virtual_private_gateway.tf
@@ -0,0 +1,16 @@
+# Create the VPG for the Transit VPC
+resource "aws_vpn_gateway" "vpn_gw" {
+    count = "${var.transit_peering_enabled}"
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    tags {
+        "transitvpc:spoke" = "true"
+    }
+}
+
+# Enable route propagation
+resource "aws_vpn_gateway_route_propagation" "backend_to_transit_vpc" {
+    count = "${var.transit_peering_enabled}"
+    vpn_gateway_id = "${aws_vpn_gateway.vpn_gw.id}"
+    route_table_id = "${aws_route_table.backend.id}"
+}
