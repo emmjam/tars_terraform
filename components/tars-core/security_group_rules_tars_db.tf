88afb5a3c62d8a4bc8a0ff7f5b934e7e7396ed3e
Mark Thompson <mark.thompson@bjss.com>
2018-04-10 15:58:20 +0100

    Add jenkinsnode to rds rule

--- components/tars-core/security_group_rules_tars_db.tf
+++ components/tars-core/security_group_rules_tars_db.tf
@@ -18,3 +18,25 @@ resource "aws_security_group_rule" "oracle_db_ingress_tars_frontend" {
   security_group_id        = "${aws_security_group.tars-core-db.id}"
   source_security_group_id = "${aws_security_group.tars-core-frontend.id}"
 }
+
+# rds DB from jenkinsnode
+resource "aws_security_group_rule" "oracle_db_ingress_jenkinsnode" {
+  description       = "Allow TCP/1521 from jenkinsnode"
+  type              = "ingress"
+  from_port         = 1521
+  to_port           = 1521
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.tars-core-db.id}"
+  source_security_group_id = "${data.terraform_remote_state.base.jenkinsnode_sg_id}"
+}
+
+# jenkinsnode to rds DB
+resource "aws_security_group_rule" "jenkinsnode_egress_oracle_db" {
+  description       = "Allow TCP/1521 to RDS"
+  type              = "egress"
+  from_port         = 1521
+  to_port           = 1521
+  protocol          = "tcp"
+  security_group_id = "${data.terraform_remote_state.base.jenkinsnode_sg_id}"
+  source_security_group_id = "${aws_security_group.tars-core-db.id}"
+}
