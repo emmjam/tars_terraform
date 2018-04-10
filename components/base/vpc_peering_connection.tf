340ea6f67479cd5a9a931aaf7469b867531169ae
Mark Thompson <mark.thompson@bjss.com>
2018-04-10 16:04:01 +0100

    Add jenkinsnode to rds rule

--- components/base/vpc_peering_connection.tf
+++ components/base/vpc_peering_connection.tf
@@ -61,12 +61,12 @@ resource "aws_vpc_peering_connection" "mgmt" {
   )}"
 }
 
-# Routes to the MGMT Account
-resource "aws_route" "private_mgmt" {
-  route_table_id            = "${aws_route_table.backend.id}"
-  destination_cidr_block    = "${lookup(var.mgmt,"vpc_cidr_block")}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt.id}"
-}
+# # Routes to the MGMT Account
+# resource "aws_route" "private_mgmt" {
+#   route_table_id            = "${aws_route_table.backend.id}"
+#   destination_cidr_block    = "${lookup(var.mgmt,"vpc_cidr_block")}"
+#   vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt.id}"
+# }
 
 resource "aws_route" "jenkins_mgmt" {
   route_table_id            = "${aws_route_table.jenkins_nat.id}"
