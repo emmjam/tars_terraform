8da39287365976a46985a1caa3fadf5dde427930
Mark Thompson <mark.thompson@bjss.com>
2018-04-10 16:32:06 +0100

    Updated tars-core

---
+++ components/tars-core/data_terraform_remote_state-ctrl.tf
@@ -0,0 +1,37 @@
+data "terraform_remote_state" "ctrl" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.tf_state_bucket_prefix}-${var.aws_account_id}-${var.aws_region}"
+
+    key = "${format(
+      "%s/%s/%s/%s/%s.tfstate",
+      var.project,
+      var.aws_account_id,
+      var.aws_region,
+      var.account_environment,
+      "ctrl"
+    )}"
+
+    region = "${var.aws_region}"
+  }
+}
+
+# data "terraform_remote_state" "ctrl" {
+#   backend = "s3"
+
+#   config {
+#     bucket = "${var.terraform_state_bucket}"
+
+#     key = "${format(
+#       "%s/%s/%s/%s/%s.tfstate",
+#       var.project,
+#       var.aws_account_id,
+#       var.region,
+#       var.account_environment,
+#       "ctrl"
+#     )}"
+
+#     region = "${var.region}"
+#   }
+# }
