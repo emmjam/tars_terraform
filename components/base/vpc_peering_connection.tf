da7508b9e988718164c4a605ffc4fa297743b8b7
Callum Massey <callum.massey@bjss.com>
2018-04-10 15:10:26 +0100

    Readd mgmt peering

--- components/base/vpc_peering_connection.tf
+++ components/base/vpc_peering_connection.tf
@@ -28,8 +28,49 @@ resource "aws_vpc_peering_connection" "ctrl" {
 }
 
 # Routes to the CTRL VPC
-resource "aws_route" "private_mgmt" {
+resource "aws_route" "private_ctrl" {
   route_table_id            = "${aws_route_table.backend.id}"
   destination_cidr_block    = "${lookup(var.mgmt,"vpc_cidr_block")}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
 }
+
+# Peer with the MGMT account
+resource "aws_vpc_peering_connection" "mgmt" {
+  vpc_id        = "${aws_vpc.vpc.id}"
+  peer_owner_id = "${lookup(var.mgmt,"aws_account_id")}"
+  peer_vpc_id   = "${lookup(var.mgmt,"vpc_id")}"
+
+  auto_accept = false
+
+  requester {
+    "allow_remote_vpc_dns_resolution" = true
+  }
+
+  tags = "${merge(
+    var.default_tags,
+    map(
+      "Name", format(
+        "%s-%s-%s/%s",
+        var.project,
+        var.environment,
+        var.component,
+        "mgmt"
+      ),
+      "Side", "Requester"
+    )
+  )}"
+}
+
+# Routes to the MGMT Account
+resource "aws_route" "private_mgmt" {
+  route_table_id            = "${aws_route_table.backend.id}"
+  destination_cidr_block    = "${lookup(var.mgmt,"vpc_cidr_block")}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt.id}"
+}
+
+resource "aws_route" "jenkins_mgmt" {
+  route_table_id            = "${aws_route_table.jenkins_nat.id}"
+  destination_cidr_block    = "${lookup(var.mgmt,"vpc_cidr_block")}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt.id}"
+}
+
