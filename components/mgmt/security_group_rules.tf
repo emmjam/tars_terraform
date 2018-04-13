ef42de9076b55973f81056675b224eb0eae9fd86
Mark Thompson <mark.thompson@bjss.com>
2018-04-13 15:47:17 +0100

    Updated whitelist

--- components/mgmt/security_group_rules.tf
+++ components/mgmt/security_group_rules.tf
@@ -52,88 +52,6 @@ resource "aws_security_group_rule" "jenkins_elb_ingress_gitlab_80" {
   source_security_group_id = "${module.gitlab.gitlab_sg_id}"
 }
 
-# jenkins-jenkinsnode
-resource "aws_security_group_rule" "jenkins_elb_ingress_jenkinsnode_80" {
-  description              = "Allow TCP/80 from JenkinsNode"
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = "${module.jenkins.elb_sg_id}"
-  source_security_group_id = "${module.jenkinsnode.security_group_id}"
-}
-
-resource "aws_security_group_rule" "jenkins_elb_ingress_jenkinsnode_49187" {
-  description              = "Allow TCP/49187 from JenkinsNode"
-  type                     = "ingress"
-  from_port                = 49187
-  to_port                  = 49187
-  protocol                 = "tcp"
-  security_group_id        = "${module.jenkins.elb_sg_id}"
-  source_security_group_id = "${module.jenkinsnode.security_group_id}"
-}
-
-resource "aws_security_group_rule" "jenkinsnode_egress_jenkins_elb_80" {
-  description              = "Allow TCP/80 to Jenkins ELB"
-  type                     = "egress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = "${module.jenkinsnode.security_group_id}"
-  source_security_group_id = "${module.jenkins.elb_sg_id}"
-}
-
-resource "aws_security_group_rule" "jenkinsnode_egress_jenkins_elb_49187" {
-  description              = "Allow TCP/49187 to Jenkins ELB"
-  type                     = "egress"
-  from_port                = 49187
-  to_port                  = 49187
-  protocol                 = "tcp"
-  security_group_id        = "${module.jenkinsnode.security_group_id}"
-  source_security_group_id = "${module.jenkins.elb_sg_id}"
-}
-
-resource "aws_security_group_rule" "jenkinsnode_egress_internet_https" {
-  description       = "Allow TCP/443 to Internet"
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = "${module.jenkinsnode.security_group_id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "jenkinsnode_egress_internet_http" {
-  description       = "Allow TCP/80 to Internet"
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = "${module.jenkinsnode.security_group_id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-# jenkinsnode-gitlab
-resource "aws_security_group_rule" "jenkinsnode_egress_gitlab_elb_private_ssh" {
-  description              = "Allow TCP/22 to GitLab Private ELB"
-  type                     = "egress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = "${module.jenkinsnode.security_group_id}"
-  source_security_group_id = "${module.gitlab.elb_private_sg_id}"
-}
-
-resource "aws_security_group_rule" "gitlab_elb_private_ingress_jenkinsnode_ssh" {
-  description              = "Allow TCP/22 from JenkinsNode"
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = "${module.gitlab.elb_private_sg_id}"
-  source_security_group_id = "${module.jenkinsnode.security_group_id}"
-}
-
 # alb-jenkins
 resource "aws_security_group_rule" "jenkins_ingress_alb_public_8080" {
   description              = "Allow TCP/8080 from ALB"
@@ -349,25 +267,4 @@ resource "aws_security_group_rule" "gitlab_egress_internet_ntp" {
   protocol          = "udp"
   security_group_id = "${module.gitlab.gitlab_sg_id}"
   cidr_blocks       = ["0.0.0.0/0"]
-}
-
-#jenkinsnode-jenkinsnode -> required to allow SSH comms w/ Packer builder instance inside jenkinsnode sg
-resource "aws_security_group_rule" "jenkinsnode_egress_self_ssh" {
-  description       = "Allow TCP/22 to JenkinsNode"
-  type              = "egress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  self              = "true"
-  security_group_id = "${module.jenkinsnode.security_group_id}"
-}
-
-resource "aws_security_group_rule" "jenkinsnode_ingress_self_ssh" {
-  description       = "Allow TCP/22 from JenkinsNode"
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  self              = "true"
-  security_group_id = "${module.jenkinsnode.security_group_id}"
 }
