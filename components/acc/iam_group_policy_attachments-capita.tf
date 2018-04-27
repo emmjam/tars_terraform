31445245731d40b670235c635796b91df46913c7
Mark Thompson <mark.thompson@bjss.com>
2018-04-27 09:23:16 +0100

    Updates to make acc in mgmt work

--- components/acc/iam_group_policy_attachments-capita.tf
+++ components/acc/iam_group_policy_attachments-capita.tf
@@ -5,6 +5,7 @@ resource "aws_iam_group_policy_attachment" "capita_users" {
 }
 
 resource "aws_iam_group_policy_attachment" "capita_users_enforce_mfa" {
+  count      = "${length(var.capita_users) == 0 ? 0 : 1}"
   group      = "${aws_iam_group.capita_users.name}"
   policy_arn = "${aws_iam_policy.enforce_mfa.arn}"
 }
