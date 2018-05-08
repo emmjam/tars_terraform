c38f005748eb44421f2b338ea6af0898734692e8
Callum Massey <callum.massey@bjss.com>
2018-05-08 12:45:25 +0100

    fix cloudtrail

--- components/acc/module_cwles-cloudtrail.tf
+++ components/acc/module_cwles-cloudtrail.tf
@@ -7,6 +7,8 @@ module "cwles_cloudtrail" {
 
   aws_account_id = "${var.aws_account_id}"
 
+  cwles_logstoes_es_domain_endpoint = "${module.cwles.es_domain_endpoint}"
+
   cwles_logstoes_lambda_arn = "${module.cwles.logstoes_lambda_arn}"
   s3_bucketlogs_bucket_id   = "${aws_s3_bucket.bucketlogs.id}"
 
