00601069135a94f7a3f3e050fd6d46eef0176b3e
Callum Massey <callum.massey@bjss.com>
2018-05-10 15:14:37 +0100

    Fix nonat ctrl routing

--- components/base/routes_ctrl.tf
+++ components/base/routes_ctrl.tf
@@ -4,3 +4,29 @@ resource "aws_route" "public_ctrl" {
   vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
 }
 
+resource "aws_route" "private_nat_ctrl" {
+  count                     = "${length(var.squidnat_subnets_cidrs)}"
+  route_table_id            = "${element(aws_route_table.private_nat.*.id,count.index)}"
+  destination_cidr_block    = "${data.terraform_remote_state.ctrl.vpc_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
+}
+
+resource "aws_route" "ctrl_private_nat_base" {
+  route_table_id            = "${data.terraform_remote_state.ctrl.rt_private_nat_id}"
+  destination_cidr_block    = "${aws_vpc.vpc.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
+}
+
+resource "aws_route" "ctrl_private_base" {
+  route_table_id            = "${data.terraform_remote_state.ctrl.rt_private_id}"
+  destination_cidr_block    = "${aws_vpc.vpc.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
+}
+
+resource "aws_route" "private_nonat_ctrl" {
+  route_table_id            = "${aws_route_table.private_nonat.id}"
+  destination_cidr_block    = "${data.terraform_remote_state.ctrl.vpc_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
+}
+
+
--- components/base/vpc_peering_connection.tf
+++ components/base/vpc_peering_connection.tf
@@ -54,28 +54,3 @@ resource "aws_vpc_peering_connection" "mgmt" {
   )}"
 }
 
-resource "aws_route" "jenkins_mgmt" {
-  count                     = "${length(var.squidnat_subnets_cidrs)}"
-  route_table_id            = "${element(aws_route_table.private_nat.*.id,count.index)}"
-  destination_cidr_block    = "${var.mgmt_vpc_cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt.id}"
-}
-
-resource "aws_route" "jenkins_ctrl" {
-  count                     = "${length(var.squidnat_subnets_cidrs)}"
-  route_table_id            = "${element(aws_route_table.private_nat.*.id,count.index)}"
-  destination_cidr_block    = "${data.terraform_remote_state.ctrl.vpc_cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
-}
-
-resource "aws_route" "private_nat_ctrl" {
-  route_table_id            = "${data.terraform_remote_state.ctrl.rt_private_nat_id}"
-  destination_cidr_block    = "${aws_vpc.vpc.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
-}
-
-resource "aws_route" "private_ctrl_bastion" {
-  route_table_id            = "${data.terraform_remote_state.ctrl.rt_private_id}"
-  destination_cidr_block    = "${aws_vpc.vpc.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.ctrl.id}"
-}
