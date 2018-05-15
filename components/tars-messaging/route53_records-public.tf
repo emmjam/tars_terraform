fdcc0056a59ed7c23270580430f4ad17785b64a1
Mark Thompson <mark.thompson@bjss.com>
2018-05-15 14:56:20 +0100

    Added ELB for RDP access

--- components/tars-messaging/route53_records-public.tf
+++ components/tars-messaging/route53_records-public.tf
@@ -16,3 +16,22 @@ resource "aws_route53_record" "tars-messaging-public" {
     evaluate_target_health = true
   }
 }
+
+# Create the R53 record for the public Messaging ELB for RDP
+resource "aws_route53_record" "tars-messaging-rdp-public" {
+  name = "${format(
+    "%s-%s-%s",
+    var.project,
+    var.environment,
+    "rdp"
+  )}"
+
+  zone_id = "${data.terraform_remote_state.acc.public_domain_name_zone_id}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.messaging_public.dns_name}"
+    zone_id                = "${aws_elb.messaging_public.zone_id}"
+    evaluate_target_health = true
+  }
+}
