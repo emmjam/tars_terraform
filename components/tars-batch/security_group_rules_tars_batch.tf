8311793dc092ac156bd73f2ea931fa345e8db5ce
Brian Collinson <brian.collinson@bjss.com>
2018-06-12 15:00:06 +0100

    Added SG rules for Batch <--> MQ

--- components/tars-batch/security_group_rules_tars_batch.tf
+++ components/tars-batch/security_group_rules_tars_batch.tf
@@ -48,3 +48,24 @@ resource "aws_security_group_rule" "oracle_db_ingress_tars_batch" {
   security_group_id        = "${data.terraform_remote_state.tars-core.tars-core-db-sg-id}"
   source_security_group_id = "${aws_security_group.tars-batch.id}"
 }
+
+resource "aws_security_group_rule" "active_mq_ingress_tars_batch" {
+  description              = "Allow TCP/61617 from TARS Batch"
+  type                     = "ingress"
+  from_port                = 61617
+  to_port                  = 61617
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.base.awsmq_sg_id}"
+  source_security_group_id = "${aws_security_group.tars-batch.id}"
+}
+
+
+resource "aws_security_group_rule" "tars_batch_egress_active_mq" {
+  description              = "Allow TCP/61617 to AWS MQ"
+  type                     = "egress"
+  from_port                = 61617
+  to_port                  = 61617
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.tars-batch.id}"
+  source_security_group_id = "${data.terraform_remote_state.base.awsmq_sg_id}"
+}
