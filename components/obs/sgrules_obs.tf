35b97592332e20674972b77b48e1967e11890624
Steven Wilson <steven.wilson@bjss.com>
2018-06-18 09:04:33 +0100

    Add OBS SG rule for TARS backend and DB

--- components/obs/sgrules_obs.tf
+++ components/obs/sgrules_obs.tf
@@ -7,3 +7,22 @@ resource "aws_security_group_rule" "obs-ingress-obs-alb-8080" {
   source_security_group_id = "${aws_security_group.obs-alb.id}"
 }
 
+resource "aws_security_group_rule" "obs-egress-tars-back-8080" {
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = "${aws_security_group.obs.id}"
+  source_security_group_id = "${data.terraform_remote_state.tars-core.tars-core-backend-sg-id}"
+}
+
+resource "aws_security_group_rule" "obs_egress_oracle_db" {
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "1521"
+  to_port                  = "1521"
+  security_group_id        = "${aws_security_group.obs.id}"
+  source_security_group_id = "${data.terraform_remote_state.tars-core.tars-core-db-sg-id}"
+}
+
+
