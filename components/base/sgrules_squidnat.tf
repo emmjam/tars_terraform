2dce49c40c2c8a71bf1c6c5d16ea5268becf45ec
Callum Massey <callum.massey@bjss.com>
2018-06-20 11:04:14 +0100

    Add Explicit proxy allow rules for sftp

--- components/base/sgrules_squidnat.tf
+++ components/base/sgrules_squidnat.tf
@@ -16,3 +16,12 @@ resource "aws_security_group_rule" "squidnat_ingress_core_http" {
   security_group_id        = "${module.squidnat.security_group_id}"
 }
 
+resource "aws_security_group_rule" "squidnat_ingress_core_explicit" {
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "3128"
+  to_port                  = "3128"
+  source_security_group_id = "${aws_security_group.core.id}"
+  security_group_id        = "${module.squidnat.security_group_id}"
+}
+
--- modules/squidnat/sgrules.tf
+++ modules/squidnat/sgrules.tf
@@ -27,3 +27,16 @@ resource "aws_security_group_rule" "squidnat_egress_whitelist_http" {
     "${var.egress_whitelist}",
   ]
 }
+
+resource "aws_security_group_rule" "squidnat_egress_whitelist_ssh" {
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "22"
+  to_port           = "22"
+  security_group_id = "${aws_security_group.squidnat.id}"
+
+  cidr_blocks = [
+    "${var.egress_whitelist}",
+  ]
+}
+
