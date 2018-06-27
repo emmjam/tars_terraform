bd069837df031f6f16cbdbf196a68dfe38581ac2
Steven Wilson <steven.wilson@bjss.com>
2018-06-27 14:59:32 +0100

    Add SG connectivity from IBS to TARS BE

--- components/ibs/sgrules_ibs.tf
+++ components/ibs/sgrules_ibs.tf
@@ -16,5 +16,25 @@ resource "aws_security_group_rule" "ibs-egress-ibs-rds-3306" {
   source_security_group_id = "${aws_security_group.ibs_aurora.id}"
 }
 
+resource "aws_security_group_rule" "ibs-egress-tars-backend-8080" {
+  description              = "Allow TCP/8080 to TARS Backend"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = "${aws_security_group.ibs.id}"
+  source_security_group_id = "${data.terraform_remote_state.tars-core.tars-core-backend-alb-sg-id}"
+}
+
+resource "aws_security_group_rule" "ibs-ingress-tars-backend-8080" {
+  description              = "Allow TCP/8080 from IBS"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = "${data.terraform_remote_state.tars-core.tars-core-backend-alb-sg-id}"
+  source_security_group_id = "${aws_security_group.ibs.id}"
+}
+
 
 
