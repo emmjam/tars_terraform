1c19a7ecc96a2874b955b051ae6b52cceaf063e4
Mark Thompson <mark.thompson@bjss.com>
2018-07-05 09:08:24 +0100

    Added onprem DMS targets and RDS instances from new snapshots

---
+++ components/tars-dms/dms_rsis-endpoint-target-on-prem.tf
@@ -0,0 +1,32 @@
+# Create a source endpoint
+resource "aws_dms_endpoint" "rsistargetonprem" {
+  endpoint_id                 = "${format(
+    "%s-%s-%s-%s",
+    var.project,
+    var.environment,
+    var.component,
+    "rsistargetonprem"
+  )}"
+  endpoint_type               = "target"
+  engine_name                 = "oracle"
+  extra_connection_attributes = ""
+  port                        = 1521
+
+  tags = "${merge(
+    var.default_tags,
+    map(
+      "Name", format(
+        "%s-%s-%s/%s",
+        var.project,
+        var.environment,
+        var.component,
+        "rsistargetonprem"
+      ),
+    )
+  )}"
+
+  username = "${var.target_dms_rsis_rds_username}"
+  password = "${var.target_dms_rsis_rds_password}"
+  server_name = "${aws_db_instance.target_rsisdb_dms.address}"
+  database_name = "${var.target_dms_rsis_rds_name}"
+}
