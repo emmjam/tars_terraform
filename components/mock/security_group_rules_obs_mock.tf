8d238344abad33bec478f9d1e227eb925c0af4e8
Steven Wilson <steven.wilson@bjss.com>
2018-07-12 17:12:06 +0100

    Allow OBS to communicate with Mock Server

---
+++ components/mock/security_group_rules_obs_mock.tf
@@ -0,0 +1,10 @@
+# OBS to mock ALB
+resource "aws_security_group_rule" "obs_egress_alb_mock_port_8080" {
+  description              = "Allow TCP/8080 to TARS mock ALB"
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.obs.obs-sg-id}"
+  source_security_group_id = "${aws_security_group.tars-alb-mock.id}"
+}
