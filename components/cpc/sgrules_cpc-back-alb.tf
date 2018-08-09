8a5ecb12256879829da078a41f5f4ebd292938ee
Michael Taylor <michael.taylor@bjss.com>
2018-08-09 11:39:00 +0100

    ingress and egress rules added for cpc-back to cpc-back-alb

--- components/cpc/sgrules_cpc-back-alb.tf
+++ components/cpc/sgrules_cpc-back-alb.tf
@@ -35,3 +35,13 @@ resource "aws_security_group_rule" "cpc_back_alb_ingress_tars_back" {
   security_group_id        = "${aws_security_group.cpc-back-alb.id}"
   source_security_group_id = "${data.terraform_remote_state.tars-core.tars-core-backend-sg-id}"
 }
+
+resource "aws_security_group_rule" "cpc_back_alb_ingress_cpc_back" {
+  description              = "Allow TCP/8080 from cpc back"
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.cpc-back-alb.id}"
+  source_security_group_id = "${module.cpc-back.security_group_id}"
+}
--- components/cpc/sgrules_cpc-back.tf
+++ components/cpc/sgrules_cpc-back.tf
@@ -76,3 +76,12 @@ resource "aws_security_group_rule" "cpc_back_egress_dvla_elise" {
   security_group_id        = "${module.cpc-back.security_group_id}"
   cidr_blocks              = ["${var.dvla_elise_server}"]
 }
+resource "aws_security_group_rule" "cpc_back_egress_cpc_back_alb" {
+  description              = "Allow TCP/8080 to CPC back ALB"
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = "${module.cpc-back.security_group_id}"
+  source_security_group_id = "${aws_security_group.cpc-back-alb.id}"
+}
