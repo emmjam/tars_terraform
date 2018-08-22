bd47d57dd1d646db27dc13c729aebe0c815e17cd
Callum Massey <callum.massey@bjss.com>
2018-08-22 15:40:25 +0100

    Updates to prod for acc users management

--- components/acc/iam_users-ithc_ro_users.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_user" "ithc_ro_users" {
-  count         = "${length(var.ithc_ro_users)}"
-  name          = "${element(var.ithc_ro_users, count.index)}"
-  path          = "/"
-  force_destroy = "true"
-}
