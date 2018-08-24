c97df42584ec3ff73357b8e5ae4bc4cab889db51
Steven Wilson <steven.wilson@bjss.com>
2018-08-24 20:24:07 +0100

    Add outputs in cpc for sftpserver and batch

--- components/cpc/outputs.tf
+++ components/cpc/outputs.tf
@@ -3,3 +3,10 @@ output "cpc-back-alb" {
   value = "${aws_security_group.cpc-back-alb.id}"
 }
 
+output "subnet_cidrs_cpc_backend" {
+  value = "${module.cpc-back.subnet_ids}"
+}
+
+output "cpc-db-sg-id" {
+  value = "${aws_security_group.cpc-db.id}"
+}
