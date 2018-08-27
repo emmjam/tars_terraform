8b70bdf940817d7810b4977b8baf431c383ad8e1
Steven Wilson <steven.wilson@bjss.com>
2018-08-27 15:32:50 +0100

    Sort out Target groups for lb to be nlb compliant

--- components/sftplus-server/nlb_target_group_cpc.tf
+++ components/sftplus-server/nlb_target_group_cpc.tf
@@ -12,16 +12,28 @@ resource "aws_lb_target_group" "sftpplus_svr-15021" {
   vpc_id   = "${data.terraform_remote_state.base.vpc_id}"
 
   health_check {
-    timeout             = 5
-    interval            = 10
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
+    port                = "traffic-port"
+    protocol            = "TCP"
+    healthy_threshold   = "3"
+    unhealthy_threshold = "3"
+    interval            = "10"
   }
 
+  target_type = "instance"
+
+  tags = "${merge(
+    var.default_tags,
+    map(
+      "Name", format(
+        "%s-%s-%s/%s",
+        var.project,
+        var.environment,
+        var.component,
+        "sftpplus-svr"
+      )
+    )
+  )}"
+
 }
 
-resource "aws_autoscaling_attachment" "sftpplus_svr-15021" {
-  autoscaling_group_name = "${module.sftpplus_svr.autoscaling_group_id}"
-  alb_target_group_arn   = "${aws_lb_target_group.sftpplus_svr-15021.arn}"
-}
 
