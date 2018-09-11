53c42eb36b92c4c9590a5ed41840a33d799a6e5f
Sam Smart <sam.smart@bjss.com>
2018-09-11 18:42:13 +0100

    Update IAM policy and AMI ID for prometheus

--- components/base/iam_policy_document-prometheus.tf
+++ components/base/iam_policy_document-prometheus.tf
@@ -6,6 +6,7 @@ data "aws_iam_policy_document" "prometheus" {
     actions = [
       "cloudwatch:GetMetricStatistics",
       "cloudwatch:ListMetrics",
+      "ec2:DescribeInstances"
     ]
 
     resources = [
