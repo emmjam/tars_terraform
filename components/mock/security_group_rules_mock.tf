8e006f5a3e9893237e3f5f43156624ea373934eb
Steven Wilson <steven.wilson@bjss.com>
2018-09-12 09:42:08 +0100

    Remove duplicate rules for Mock - TARSDB connection

--- components/mock/security_group_rules_mock.tf
+++ components/mock/security_group_rules_mock.tf
@@ -20,16 +20,6 @@ resource "aws_security_group_rule" "tars_mock_ingress_public_alb_port_8443" {
   source_security_group_id = "${aws_security_group.tars-alb-mock-public.id}"
 }
 
-resource "aws_security_group_rule" "tars_mock_egress_oracle_db" {
-  description              = "Allow TCP/1521 to the Oracle DB"
-  type                     = "egress"
-  from_port                = 1521
-  to_port                  = 1521
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.tars-mock.id}"
-  source_security_group_id = "${data.terraform_remote_state.tars-core.tars-core-db-sg-id}"
-}
-
 resource "aws_security_group_rule" "tars_mock_ingress_bastion" {
   description              = "Allow TCP/22 from bastion"
   type                     = "ingress"
@@ -50,12 +40,3 @@ resource "aws_security_group_rule" "tars_mock_egress_kms_endpoint" {
   source_security_group_id = "${data.terraform_remote_state.base.kms_sg_id}"
 }
 
-resource "aws_security_group_rule" "oracle_db_ingress_tars_mock" {
-  description              = "Allow TCP/1521 from TARS mock"
-  type                     = "ingress"
-  from_port                = 1521
-  to_port                  = 1521
-  protocol                 = "tcp"
-  security_group_id        = "${data.terraform_remote_state.tars-core.tars-core-db-sg-id}"
-  source_security_group_id = "${aws_security_group.tars-mock.id}"
-}
--- components/mock/security_group_rules_mock_tarsdb.tf
+++ components/mock/security_group_rules_mock_tarsdb.tf
@@ -9,7 +9,7 @@ resource "aws_security_group_rule" "tars_db_mock_ingress_port_1521" {
   source_security_group_id = "${aws_security_group.tars-mock.id}"
 }
 
-resource "aws_security_group_rule" "mock_tars_db_egress__port_1521" {
+resource "aws_security_group_rule" "mock_tars_db_egress_port_1521" {
   description              = "Allow TCP/1521 to tars DB"
   type                     = "egress"
   from_port                = 1521
