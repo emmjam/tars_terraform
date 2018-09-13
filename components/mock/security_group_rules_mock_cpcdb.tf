5ef43fa4d60aa179e062e6fc72e6faade04dd838
Steven Wilson <steven.wilson@bjss.com>
2018-09-13 09:46:22 +0100

    Add CPC DB access to Mock

---
+++ components/mock/security_group_rules_mock_cpcdb.tf
@@ -0,0 +1,23 @@
+# TARS Mock CPC DB SG Rules
+resource "aws_security_group_rule" "cpc_db_mock_ingress_port_1521" {
+  description = "Allow TCP/1521 from Mock"
+  type = "ingress"
+  from_port = 1521
+  to_port = 1521
+  protocol = "tcp"
+  security_group_id = "${data.terraform_remote_state.cpc.cpc-db-sg-id}"
+  source_security_group_id = "${aws_security_group.tars-mock.id}"
+}
+
+resource "aws_security_group_rule" "mock_cpc_db_egress_port_1521" {
+  description              = "Allow TCP/1521 to CPC DB"
+  type                     = "egress"
+  from_port                = 1521
+  to_port                  = 1521
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.tars-mock.id}"
+  source_security_group_id = "${data.terraform_remote_state.cpc.cpc-db-sg-id}"
+}
+
+
+
