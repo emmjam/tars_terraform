ef69e0f1da3152840f2dddba8bee3dc9507188cb
Mike Peachey <mike.peachey@bjss.com>
2018-10-03 15:47:17 +0100

    chmod a-x, providers and data_aws rename

--- components/base/route53_zone_private.tf
+++ components/base/route53_zone_private.tf
@@ -2,6 +2,6 @@
 # Keep it generic so it's the same across all VPC's/envs
 resource "aws_route53_zone" "vpc" {
   name    = "${local.vpc_domain_name}"
-  comment = "Private ${var.component} ${local.vpc_domain_name} hosted zone"
+  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name} hosted zone"
   vpc_id  = "${aws_vpc.vpc.id}"
 }
