248dd32e28759dfa75a72922bf12e4135b573b8d
Callum Massey <callum.massey@bjss.com>
2018-10-17 11:42:48 +0100

    Remove Database Componenet

--- components/database/data_aws.tf
+++
@@ -1,4 +0,0 @@
-# All AWS Availability Zones in this region
-data "aws_availability_zones" "available" {}
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
--- components/database/route53_zone_private.tf
+++
@@ -1,7 +0,0 @@
-# Create the private R53 zone for the tars apps
-# Keep it generic so it's the same across all VPC's/envs
-resource "aws_route53_zone" "vpc" {
-  name    = "${var.environment}.${var.private_domain_name}"
-  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name} hosted zone"
-  vpc_id  = "${aws_vpc.vpc.id}"
-}
