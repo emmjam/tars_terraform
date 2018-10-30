ce78ad05baf5003184ca07de8ff50e15e95dd649
Sam Smart <sam.smart@bjss.com>
2018-10-30 16:33:29 +0000

    Ensure eip count source isn't from module output

--- components/sftplus-server/nlb_eips.tf
+++ components/sftplus-server/nlb_eips.tf
@@ -1,5 +1,5 @@
 #EIPs to allocate to the public NLB, one per AZ
 resource "aws_eip" "nlb_public" {
-  count = "${length(module.sftpplus_svr.subnet_ids)}"
+  count = "${length(var.cpc_sftp_subnets_cidrs)}"
   vpc   = true
 }
