46e4ab8e3ba6bb332628d646459a785ad15c7e11
Mark Thompson <mark.thompson@bjss.com>
2018-10-31 09:24:27 +0000

    Fixes all the DMS rules

--- components/tars-dms/sgrules_tars_reporting_rsis_db.tf
+++ components/tars-dms/sgrules_tars_reporting_rsis_db.tf
@@ -1,6 +1,16 @@
+resource "aws_security_group_rule" "rsis_rds_ingress_oraclexe_1521" {
+  description              = "Allow TCP/1521 inbound"
+  type                     = "ingress"
+  from_port                = 1521
+  to_port                  = 1521
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.tars-reporting.rsis-db-sg-id}"
+  source_security_group_id = "${aws_security_group.oraclexe.id}"
+}
+
 resource "aws_security_group_rule" "rsis_rds_ingress_dms_1521" {
   description              = "Allow TCP/1521 inbound from dms"
-  type                     = "egress"
+  type                     = "ingress"
   from_port                = 1521
   to_port                  = 1521
   protocol                 = "tcp"
