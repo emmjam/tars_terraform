dc70d1063bd32d043ac29308dd696f0b6289da13
Steven Wilson <steven.wilson@bjss.com>
2018-11-09 11:42:06 +0000

    Add Windows service monitoring ports to Messaging SGRules

--- components/tars-messaging/sgrules_tars_messaging_ad.tf
+++ components/tars-messaging/sgrules_tars_messaging_ad.tf
@@ -273,3 +273,56 @@ resource "aws_security_group_rule" "tars_messaging_egress_DVSA_printers" {
     "${var.dvsa_printers}",
   ]
 }
+
+resource "aws_security_group_rule" "dvsa_win_monitor_ingress_tars_messaging_ICMP" {
+  description = "Allow all ICMP from DVSA monitoring"
+  type = "ingress"
+  from_port = -1
+  to_port = -1
+  protocol = "ICMP"
+  security_group_id = "${aws_security_group.tars-messaging-ad.id}"
+
+  cidr_blocks = [
+    "${var.dvsa_icmp_monitoring}",
+  ]
+}
+
+resource "aws_security_group_rule" "dvsa_ingress_tars_messaging_win_rpc_port" {
+  description = "Allow RPC from DVSA monitoring"
+  type = "ingress"
+  from_port = 135
+  to_port = 135
+  protocol = "TCP"
+  security_group_id = "${aws_security_group.tars-messaging-ad.id}"
+
+  cidr_blocks = [
+    "${var.dvsa_win_monitoring}",
+  ]
+}
+
+resource "aws_security_group_rule" "dvsa_ingress_tars_messaging_win_smb_port" {
+  description = "Allow SMB from DVSA monitoring"
+  type = "ingress"
+  from_port = 445
+  to_port = 445
+  protocol = "TCP"
+  security_group_id = "${aws_security_group.tars-messaging-ad.id}"
+
+  cidr_blocks = [
+    "${var.dvsa_win_monitoring}",
+  ]
+}
+
+resource "aws_security_group_rule" "dvsa_ingress_tars_messaging_win_dynamic_ports" {
+  description = "Allow all Dynamic Ports from DVSA monitoring"
+  type = "ingress"
+  from_port = 49152
+  to_port = 65535
+  protocol = "TCP"
+  security_group_id = "${aws_security_group.tars-messaging-ad.id}"
+
+  cidr_blocks = [
+    "${var.dvsa_win_monitoring}",
+  ]
+}
+
