5807541427fc3cda44360c87cb00dc44d2c31985
Sam Smart <sam.smart@bjss.com>
2019-01-22 15:06:58 +0000

    Add aws mq failover DNS entry

--- components/base/route53_records_private.tf
+++ components/base/route53_records_private.tf
@@ -1,25 +1,28 @@
 # R53 record for the AWS MQ endpoint
-
 resource "aws_route53_record" "awsmq" {
+  count = "${length(aws_mq_broker.tars-awsmq.instances)}"
+
   name = "${format(
-    "%s-%s",
+    "%s-%s-%s",
     var.project,
-    "awsmq"
+    "awsmq",
+    "${count.index}"
   )}"
 
   zone_id = "${aws_route53_zone.vpc.zone_id}"
   type    = "CNAME"
   ttl     = 60
 
-  # aws_mq_broker doesn't provide a direct endpoint that's ready to be used by R53
-  # aws_mq_broker.tars-awsmq.instances.0.endpoints.0 should provide something like ssl://b-4e6a583f-ccc8-4e6e-a873-f365df685ddb-1.mq.eu-west-1.amazonaws.com:61617
-  # So strip off the first 6 and last 6 characters (well 6 + 6) to give
-  # b-4e6a583f-ccc8-4e6e-a873-f365df685ddb-1.mq.eu-west-1.amazonaws.com
+  # Terraform's support for list and map data handling pre 0.12.* 
+  # is terrible. We manually construct the DNS entry required until
+  # we can either upgrade to 0.12 for better splat/lookup support
+  # or we decide it's safe to use the IPs provided by aws_mq_broker
   records = [
-    "${substr(
-      aws_mq_broker.tars-awsmq.instances.0.endpoints.0,
-      6,
-      length(aws_mq_broker.tars-awsmq.instances.0.endpoints.0) - 12
-    )}",
+    "${format(
+      "%s-%d.mq.%s.amazonaws.com",
+      aws_mq_broker.tars-awsmq.id,
+      count.index + 1,
+      var.aws_region
+    )}"
   ]
 }
