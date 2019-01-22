cf30cc4cdad228c99295d82d45988560b59ef37f
Mark Thompson <mark.thompson@bjss.com>
2019-01-22 15:09:56 +0000

    Removed 9990 wildfly admin port from jenkins to messaging server. No longer required.

--- components/tars-messaging/sgrules_base_jenkinsnode.tf
+++ components/tars-messaging/sgrules_base_jenkinsnode.tf
@@ -8,14 +8,3 @@ resource "aws_security_group_rule" "jenkinsnode_egress_win_msg_ssh" {
   security_group_id        = "${data.terraform_remote_state.base.jenkinsnode_sg_id}"
   source_security_group_id = "${aws_security_group.tars-messaging.id}"
 }
-
-# jenkinsnode to windows messaging to support code/config deployments (Wildfly Admin)
-resource "aws_security_group_rule" "jenkinsnode_egress_win_msg_wildfly" {
-  description              = "Allow TCP/9990 to Windows Messaging"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = "9990"
-  to_port                  = "9990"
-  security_group_id        = "${data.terraform_remote_state.base.jenkinsnode_sg_id}"
-  source_security_group_id = "${aws_security_group.tars-messaging.id}"
-}
