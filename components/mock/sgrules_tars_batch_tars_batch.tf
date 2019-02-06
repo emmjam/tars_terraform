e3218fe41f96b6c1bbca9f2d91785c80a14793fc
Mark Thompson <mark.thompson@bjss.com>
2019-02-06 10:19:29 +0000

    Added tars-batch and tars-frontend access to mock on on port 8080

--- components/mock/sgrules_tars_batch_tars_batch.tf
+++ components/mock/sgrules_tars_batch_tars_batch.tf
@@ -10,3 +10,14 @@ resource "aws_security_group_rule" "mock_tars_batch_egress_port_22" {
     "${data.terraform_remote_state.base.subnet_cidrs_tars_backend}",
   ]
 }
+
+# TARS batch to mock ALB
+resource "aws_security_group_rule" "tars_batch_egress_tars_alb_mock_port_8080" {
+  description              = "Allow TCP/8080 to TARS mock ALB"
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.tars-batch.tars-batch-sg-id}"
+  source_security_group_id = "${aws_security_group.tars-alb-mock.id}"
+}
---
+++ components/mock/sgrules_tars_core_tars_core_frontend.tf
@@ -0,0 +1,10 @@
+# TARS frontend to mock ALB
+resource "aws_security_group_rule" "tars_frontend_egress_tars_alb_mock_port_8080" {
+  description              = "Allow TCP/8080 to TARS mock ALB"
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.tars-core.tars-core-frontend-sg-id}"
+  source_security_group_id = "${aws_security_group.tars-alb-mock.id}"
+}
