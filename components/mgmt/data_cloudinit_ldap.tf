e5856c409c5391d181e09d737e5dbe125f7ec7ec
Sam Smart <sam.smart@bjss.com>
2019-03-18 11:11:11 +0000

    Add LDAP Infra

---
+++ components/mgmt/data_cloudinit_ldap.tf
@@ -0,0 +1,60 @@
+data "template_file" "cloudinit_config_facter_custom_ldap" {
+  template = "${file("${path.module}/templates/cloudinit_config_facter_custom.yaml.tmpl")}"
+
+  vars {
+    AWS_ACCOUNT_ID = "${var.aws_account_id}"
+    AWS_REGION     = "${var.aws_region}"
+    COMPONENT      = "${var.component}"
+    ENVIRONMENT    = "${var.environment}"
+    NODETYPE       = "ldap"
+    PROJECT        = "${var.project}"
+  }
+}
+
+data "template_file" "cloudinit_config_hostname_ldap" {
+  template = "${file("${path.module}/templates/cloudinit_config_hostname_microservice.yaml.tmpl")}"
+
+  vars {
+    NODETYPE        = "ldap"
+    VPC_DOMAIN_NAME = "${local.vpc_domain_name}"
+  }
+}
+
+# The ldap cloud-init config as rendered to be user-data input
+data "template_cloudinit_config" "ldap" {
+  gzip          = true
+  base64_encode = true
+
+  # LDAP-specific template
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.cloudinit_config_hostname_ldap.rendered}"
+  }
+
+  # LDAP-specific template
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.cloudinit_config_facter_custom_ldap.rendered}"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # Account-specific template
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.cloudinit_config_eyaml.rendered}"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # Generic facts template
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.cloudinit_config_facter_generic.rendered}"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # Generic bootstrap script
+  part {
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/files/cloudinit_bootstrap_microservice.sh")}"
+  }
+}
--- components/mgmt/route53_records_private.tf
+++ components/mgmt/route53_records_private.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "nexus_private" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "ldap_private" {
+  name    = "ldap"
+  zone_id = "${aws_route53_zone.mgmt.id}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.ldap.dns_name}"
+    zone_id                = "${aws_elb.ldap.zone_id}"
+    evaluate_target_health = true
+  }
+}
