93d3a3ab72d5ba22b46bc76e10b7a8b00fd61351
Callum Massey <callum.massey@bjss.com>
2019-06-20 16:09:55 +0100

    Set maximum auth duration to 8 hours ( 1 work day)

--- components/acc/iam_role_ops.tf
+++ components/acc/iam_role_ops.tf
@@ -1,6 +1,8 @@
 resource "aws_iam_role" "ops" {
   name     = "${local.csi}-ops"
 
+  max_session_duration = "28800"
+
   assume_role_policy = "${data.aws_iam_policy_document.iam_users_assumerole.json}"
 }
 
