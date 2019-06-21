d2a28fbf63734e13e18636264fe25ce726bacbbe
Callum Massey <callum.massey@bjss.com>
2019-06-21 12:15:10 +0100

    Update alb to ccept public host header and certificate

--- components/apps/alb_listener_rule_ibs.tf
+++ components/apps/alb_listener_rule_ibs.tf
@@ -27,3 +27,18 @@ resource "aws_lb_listener_rule" "ibs-101" {
     values = ["driverpracticaltest.${data.terraform_remote_state.acc.public_domain_name}"]
   }
 }
+
+resource "aws_lb_listener_rule" "ibs-102" {
+  listener_arn = "${aws_alb_listener.apps-8080.arn}"
+  priority     = "102"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.ibs-8080.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["driverpracticaltest.dvsa.gov.uk"]
+  }
+}
