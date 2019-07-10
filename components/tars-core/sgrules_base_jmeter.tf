ccf3edd695ba31e898fc25e5371d0f4d5b96d71a
Steven Wilson <steven.wilson@bjss.com>
2019-07-10 14:31:37 +0100

    Add JMeter Connectivity to API (MES)

--- components/tars-core/sgrules_base_jmeter.tf
+++ components/tars-core/sgrules_base_jmeter.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "tars_alb_private_egress_whitelist_port_443" 
   security_group_id        = "${data.terraform_remote_state.base.jmeter_sg_id}"
   source_security_group_id = "${aws_security_group.tars-alb-private.id}"
 }
+
+resource "aws_security_group_rule" "jmeter_egress_api_8443" {
+  description              = "Allow TCP/8443 to API"
+  type                     = "egress"
+  from_port                = 8443
+  to_port                  = 8443
+  protocol                 = "tcp"
+  security_group_id        = "${data.terraform_remote_state.base.jmeter_sg_id}"
+  source_security_group_id = "${aws_security_group.tars-alb-backend-api.id}"
+}
