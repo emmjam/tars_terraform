07bac18f9ac52d11c725eafd2dba82eed09cde93
tribbickm <michael.tribbick@bjss.com>
2019-07-17 10:26:07 +0100

    aws_backup

---
+++ components/cpc-batch/aws_backup_plan_selection.tf
@@ -0,0 +1,14 @@
+resource "aws_backup_selection" "tars_aws_backup" {
+  count        = "${var.environment == "opsdev" ? 1 : 0}"
+  //count        = "${contains(["opsdev", "dev01", "sit01"], "var.environment") ? 1 : 0}" v12 not available
+  iam_role_arn = "${data.terraform_remote_state.ctrl.tars_aws_backup_iam_role_arn}"
+  name         = "${var.environment}_cpc_batch_tars_aws_backup"
+  plan_id      = "${data.terraform_remote_state.ctrl.tars_aws_backup_iam_tars_efs_id}"
+
+    selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "Name"
+    value = "tars-${var.environment}-cpc/cpc-batch-efs"
+  }
+
+}
---
+++ components/ctrl/aws_backup_plan.tf
@@ -0,0 +1,51 @@
+//Creates KMS for Backup
+resource "aws_kms_key" "tars_efs" {
+  description             = "KMS tars_efs Backup"
+  deletion_window_in_days = 10
+}
+
+//Creates Vault for Backup
+resource "aws_backup_vault" "tars_efs" {
+  name        = "tars_efs"
+  kms_key_arn = "${aws_kms_key.tars_efs.arn}"
+}
+
+//Creats Backup Plan and schedule
+resource "aws_backup_plan" "tars_efs" {
+  name = "tars_backup_plan"
+
+  rule {
+    rule_name         = "tars_efs_backup_rule"
+    target_vault_name = "${aws_backup_vault.tars_efs.name}"
+    schedule          = "cron(0 03 * * ? *)"
+    lifecycle {
+            delete_after = 30
+        }
+  }
+}
+
+// Create IAM Role for backup
+
+resource "aws_iam_role" "tars_aws_backup" {
+  name               = "tars_aws_backup"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["sts:AssumeRole"],
+      "Effect": "allow",
+      "Principal": {
+        "Service": ["backup.amazonaws.com"]
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "tars_aws_backup" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = "${aws_iam_role.tars_aws_backup.name}"
+}
+
