6adced8d953043d3c9b94ac8d333b67a4f31c986
Callum Massey <callum.massey@bjss.com>
2019-08-06 11:37:39 +0100

    add sgrules to allow nexus access to mgmto

---
+++ components/mgmt/sg_nexus_alb.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "nexus_alb" {
+  name        = "${local.csi}-nexus-alb"
+  description = "${local.csi}-nexus-alb"
+  vpc_id      = "${aws_vpc.mgmt.id}"
+
+  tags = "${merge(
+    local.default_tags,
+    map(
+      "Name", "${local.csi}-nexus-alb"
+    )
+  )}"
+}
