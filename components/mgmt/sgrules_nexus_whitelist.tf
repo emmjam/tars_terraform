16e97d3e5ca04c12525874f59df178777ec0ad1b
Callum Massey <callum.massey@bjss.com>
2019-08-06 12:56:40 +0100

    Update to use correct nat IPs

--- components/mgmt/sgrules_nexus_whitelist.tf
+++ components/mgmt/sgrules_nexus_whitelist.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group_rule" "nexus_ingress_nat_https" {
-  count             = "${length(aws_eip.nat.*.id)}"
+  count             = "${length(data.terraform_remote_state.ctrl.eip_nat_public_ips)}"
   type              = "ingress"
   protocol          = "tcp"
   from_port         = "443"
@@ -7,7 +7,7 @@ resource "aws_security_group_rule" "nexus_ingress_nat_https" {
   security_group_id = "${aws_security_group.nexus_alb.id}"
 
   cidr_blocks = [
-    "${element(aws_eip.nat.*.public_ip,count.index)}/32",
+    "${element(data.terraform_remote_state.ctrl.eip_nat_public_ips,count.index)}/32",
   ]
 }
 
