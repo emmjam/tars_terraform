4bea46d0480c1cd0189734fe4783550d5a496b80
Stephen.Davies <stephen.davies@bjss.com>
2019-12-17 16:18:00 +0000

    New Support Access policy attachment - full RO

--- components/acc/iam_role_policy_attachment_readonly.tf
+++ components/acc/iam_role_policy_attachment_readonly.tf
@@ -2,3 +2,8 @@ resource "aws_iam_role_policy_attachment" "readonly_access" {
   role       = "${aws_iam_role.readonly.name}"
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "readonly_support_access" {
+  role       = "${aws_iam_role.readonly.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSSupportAccess"
+}
