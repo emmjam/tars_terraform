c42146014e91fee15279654d0f1901c6d127847e
Callum Massey <callum.massey@bjss.com>
2020-01-20 12:17:09 +0000

    add builder iam role

---
+++ components/mgmt/data_iam_policy_document_builder.tf
@@ -0,0 +1,16 @@
+# TODO: adjust permissions based on real usage
+data "aws_iam_policy_document" "builder" {
+  statement {
+    sid    = "AllowAccountManagement"
+    effect = "Allow"
+
+    actions = [
+      "*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
