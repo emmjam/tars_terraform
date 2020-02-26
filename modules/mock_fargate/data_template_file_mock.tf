3ba3209589c92456b28888c82a9b97330ecd976b
Callum Massey <callum.massey@bjss.com>
2020-02-26 17:21:16 +0000

    Add new env variable to container

--- modules/mock_fargate/data_template_file_mock.tf
+++ modules/mock_fargate/data_template_file_mock.tf
@@ -10,5 +10,6 @@ data "template_file" "task" {
     log_group     = aws_cloudwatch_log_group.main.name
     stream_prefix = "${local.csi}-ecs"
     name          = "${var.name}"
+    response_domain = "mock-epdq-${var.environment}-public.dvsa.tars.dev-dvsacloud.uk"
   }
 }
