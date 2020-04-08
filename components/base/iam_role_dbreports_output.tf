2fd404fa9610cb4a3dfb89bf7230839286bc090b
Jamie Kelly <jamie.kelly@bjss.co.uk>
2020-04-08 13:23:25 +0100

    Upgrade of syntax to tf 0.12

--- components/base/iam_role_dbreports_output.tf
+++ components/base/iam_role_dbreports_output.tf
@@ -13,26 +13,26 @@ data "aws_iam_policy_document" "dbreports_output_assume_role" {
 }
 
 resource "aws_iam_role" "dbreports_output" {
-  name = "${format(
+  name = format(
     "%s-%s-%s-%s",
     var.project,
     var.environment,
     var.component,
     "db-reports"
-  )}"
+  )
 
-  assume_role_policy = "${data.aws_iam_policy_document.dbreports_output_assume_role.json}"
+  assume_role_policy = data.aws_iam_policy_document.dbreports_output_assume_role.json
 }
 
 resource "aws_iam_role_policy" "dbreports_output" {
-  name = "${format(
+  name = format(
     "%s-%s-%s-%s",
     var.project,
     var.environment,
     var.component,
     "db-reports"
-  )}"
+  )
 
-  role   = "${aws_iam_role.dbreports_output.id}"
-  policy = "${data.aws_iam_policy_document.dbreports_output.json}"
+  role   = aws_iam_role.dbreports_output.id
+  policy = data.aws_iam_policy_document.dbreports_output.json
 }
