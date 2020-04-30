f582c9a10ad784b45f831da00bafaa111c6b5171
Callum Massey <callum.massey@bjss.com>
2020-04-30 16:25:12 +0100

    Add mgmt natgw to nexus whitelist

--- components/mgmt/sgrules_nexus_whitelist.tf
+++ components/mgmt/sgrules_nexus_whitelist.tf
@@ -14,3 +14,19 @@ resource "aws_security_group_rule" "nexus_ingress_nat_https" {
   ]
 }
 
+resource "aws_security_group_rule" "nexus_ingress_nat_mgmt_https" {
+  count             = length(aws_eip.nat.*.id)
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "443"
+  to_port           = "443"
+  security_group_id = aws_security_group.nexus_alb.id
+
+  cidr_blocks = [
+    "${element(
+      aws_eip.nat.*.public_ip,
+      count.index,
+    )}/32",
+  ]
+}
+
