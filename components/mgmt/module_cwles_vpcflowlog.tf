5bbaf57e5889b953dc484d50754fee6c7046dea7
Seun Adiomowo <seun.adiomowo@bjss.com>
2020-05-27 15:54:58 +0100

    TSCI-3732 mgmt flowlogs

---
+++ components/mgmt/module_cwles_vpcflowlog.tf
@@ -0,0 +1,10 @@
+module "cwles_vpcflowlog" {
+  source = "../../modules/cwles-vpcflowlog"
+
+  project     = var.project
+  environment = var.environment
+  component   = var.component
+
+  vpc_id = aws_vpc.ctrl.id
+}
+
