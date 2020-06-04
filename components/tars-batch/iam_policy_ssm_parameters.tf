0b6dcb59ef8dd11a02be7e27a90175b03b2fc981
Michelle Pike <mailto:michelle.pike@bjss.com>
2020-06-04 08:22:22 +0000

    TARS core/batch IAM roles to access ePDQ Params
    TSCI-3803

---
+++ components/tars-batch/iam_policy_ssm_parameters.tf
@@ -0,0 +1,19 @@
+# Policy to get  SSM Parameters
+data "aws_iam_policy_document" "ssm_parameters" {
+  statement {
+    sid = "AllowGetSSMPars"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-epdq-*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ssm_parameters" {
+  name        = "${local.csi}-ssm_parameters"
+  description = "IAM policy for ${local.csi}-ssm_parameters"
+  policy      = data.aws_iam_policy_document.ssm_parameters.json
+}
--- components/tars-core/iam_role_policy_attachments_tars_front.tf
+++ components/tars-core/iam_role_policy_attachments_tars_front.tf
@@ -8,3 +8,7 @@ resource "aws_iam_role_policy_attachment" "tars_front_cloudwatch_tars_core" {
   policy_arn = data.terraform_remote_state.base.outputs.iam_policy_cloudwatch_arn
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_parameters_front" {
+  role       = module.tars_front.iam_role_name
+  policy_arn = aws_iam_policy.ssm_parameters.arn
+}
