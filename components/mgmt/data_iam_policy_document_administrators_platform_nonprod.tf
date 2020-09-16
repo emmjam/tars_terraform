b42a7f06a4d92c1a829008f8f2cf55e7215c45d7
Nadeem Khan <nadeem.khan@bjss.com>
2020-09-16 15:20:46 +0100

    Removing administrator access to managment account

--- components/mgmt/data_iam_policy_document_administrators_platform_nonprod.tf
+++ components/mgmt/data_iam_policy_document_administrators_platform_nonprod.tf
@@ -8,7 +8,6 @@ data "aws_iam_policy_document" "assume_administrators_role_platform_nonprod" {
     ]
 
     resources = [
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/Administrators",
       "arn:aws:iam::652856684323:role/Administrators",
     ]
   }
