442c187bcd6b638bec7ebf0d6c0c6234c6fb6e50
Jamie Kelly <Jamie.Kelly@bjss.com>
2020-11-23 10:44:24 +0000

    Fix condition.0.values deprecation

--- components/apps/alb_listener_rule_ibs.tf
+++ components/apps/alb_listener_rule_ibs.tf
@@ -10,22 +10,15 @@ resource "aws_lb_listener_rule" "ibs-100" {
   }
 
   condition {
-    field = "host-header"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
-    values = [format(
-      "%s-%s-%s.%s",
-      "ibs",
-      var.environment,
-      "public",
-      data.terraform_remote_state.acc.outputs.public_domain_name,
-    )]
+    host_header {
+      values = [format(
+        "%s-%s-%s.%s",
+        "ibs",
+        var.environment,
+        "public",
+        data.terraform_remote_state.acc.outputs.public_domain_name,
+      )]
+    }
   }
 
   condition {
@@ -46,22 +39,15 @@ resource "aws_lb_listener_rule" "ibs-101" {
   }
 
   condition {
-    field = "host-header"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
-    values = [format(
-      "%s-%s-%s.%s",
-      "ibs",
-      var.environment,
-      "public",
-      data.terraform_remote_state.acc.outputs.public_domain_name,
-    )]
+    host_header {
+      values = [format(
+        "%s-%s-%s.%s",
+        "ibs",
+        var.environment,
+        "public",
+        data.terraform_remote_state.acc.outputs.public_domain_name,
+      )]
+    }
   }
 }
 
@@ -77,8 +63,9 @@ resource "aws_lb_listener_rule" "ibs-102" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["driverpracticaltest.${data.terraform_remote_state.acc.outputs.public_domain_name}"]
+    host_header {
+      values = ["driverpracticaltest.${data.terraform_remote_state.acc.outputs.public_domain_name}"]
+    }
   }
   condition {
     path_pattern {
@@ -98,8 +85,9 @@ resource "aws_lb_listener_rule" "ibs-103" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["driverpracticaltest.${data.terraform_remote_state.acc.outputs.public_domain_name}"]
+    host_header {
+      values = ["driverpracticaltest.${data.terraform_remote_state.acc.outputs.public_domain_name}"]
+    }
   }
 }
 
@@ -115,8 +103,9 @@ resource "aws_lb_listener_rule" "ibs-104" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["driverpracticaltest.dvsa.gov.uk"]
+    host_header {
+      values = ["driverpracticaltest.dvsa.gov.uk"]
+    }
   }
 
   condition {
@@ -137,8 +126,9 @@ resource "aws_lb_listener_rule" "ibs-105" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["driverpracticaltest.dvsa.gov.uk"]
+    host_header {
+      values = ["driverpracticaltest.dvsa.gov.uk"]
+    }
   }
 }
 
@@ -154,14 +144,15 @@ resource "aws_lb_listener_rule" "ibs-106" {
   }
 
   condition {
-    field = "host-header"
-    values = [format(
-      "%s-%s-%s.%s",
-      "incapsula-ibs",
-      var.environment,
-      "public",
-      data.terraform_remote_state.acc.outputs.public_domain_name,
-    )]
+    host_header {
+      values = [format(
+        "%s-%s-%s.%s",
+        "incapsula-ibs",
+        var.environment,
+        "public",
+        data.terraform_remote_state.acc.outputs.public_domain_name,
+      )]
+    }
   }
 
   condition {
@@ -182,14 +173,15 @@ resource "aws_lb_listener_rule" "ibs-107" {
   }
 
   condition {
-    field = "host-header"
-    values = [format(
-      "%s-%s-%s.%s",
-      "incapsula-ibs",
-      var.environment,
-      "public",
-      data.terraform_remote_state.acc.outputs.public_domain_name,
-    )]
+    host_header {
+      values = [format(
+        "%s-%s-%s.%s",
+        "incapsula-ibs",
+        var.environment,
+        "public",
+        data.terraform_remote_state.acc.outputs.public_domain_name,
+      )]
+    }
   }
 }
 
