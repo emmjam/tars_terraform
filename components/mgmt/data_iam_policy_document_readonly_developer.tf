b7c5395329cc8115056e96099a480fc8fb0bbcab
Irfan Sidat <irfan.sidat@bjss.com>
2021-01-20 16:40:42 +0000

    Allow developers to assume read only role in mgmt account

--- components/mgmt/data_iam_policy_document_readonly_developer.tf
+++ components/mgmt/data_iam_policy_document_readonly_developer.tf
@@ -9,6 +9,7 @@ data "aws_iam_policy_document" "assume_readonly_role_developer" {
 
     resources = [
       "arn:aws:iam::652856684323:role/ReadOnly",
+      "arn:aws:iam::645711882182:role/ReadOnly",
       "arn:aws:iam::246976497890:role/ReadOnlyNoSecrets"
     ]
   }
