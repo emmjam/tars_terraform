ca871f8ef8ca8a4d33311ef3600d1deee2f2ccca
John Morley <john.morley@bjss.com>
2021-03-24 17:40:19 +0000

    Correct mock ALB ingress/egress to CPC back

--- components/mock/sgrules_mock_fargate.tf
+++ components/mock/sgrules_mock_fargate.tf
@@ -125,7 +125,7 @@ resource "aws_security_group_rule" "cpc_egress_mock_alb" {
   to_port                  = 443
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.tars-alb-mock.id
-  security_group_id        = data.terraform_remote_state.cpc.outputs.cpc-back-sg-id
+  security_group_id        = data.terraform_remote_state.cpc.outputs.cpc-front-sg-id
 }
 
 resource "aws_security_group_rule" "mock_alb_ingress_cpc" {
@@ -134,7 +134,7 @@ resource "aws_security_group_rule" "mock_alb_ingress_cpc" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.cpc.outputs.cpc-back-sg-id
+  source_security_group_id = data.terraform_remote_state.cpc.outputs.cpc-front-sg-id
   security_group_id        = aws_security_group.tars-alb-mock.id
 }
 
