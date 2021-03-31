4b3eef77dc244969fe9cd39f2c3b7eb51caee19f
John Morley <john.morley@bjss.com>
2021-03-31 15:26:47 +0100

    Add CPC DE to mock GovGateway redirect URL whitelist

--- modules/mock_fargate/data_template_file_mock.tf
+++ modules/mock_fargate/data_template_file_mock.tf
@@ -14,7 +14,7 @@ data "template_file" "task" {
     gov_gateway_log_group      = aws_cloudwatch_log_group.gov_gateway.name
     gov_gateway_stream_prefix  = "${local.csi}-ecs-gov-gateway"
     gov_gateway_name           = local.gov_gateway_name
-    gov_gateway_auth_callbacks = "https://tars-${var.environment}-cpc-internet.dvsa.tars.dev-dvsacloud.uk/cpctrain/login?app=CPCER, https://obs-${var.environment}-public.dvsa.tars.dev-dvsacloud.uk/obs-web/login?app=OBS"
+    gov_gateway_auth_callbacks = "https://tars-${var.environment}-cpc-internet.dvsa.tars.dev-dvsacloud.uk/cpctrain/login?app=CPCER, https://obs-${var.environment}-public.dvsa.tars.dev-dvsacloud.uk/obs-web/login?app=OBS, https://tars-${var.environment}-cpc-internet.dvsa.tars.dev-dvsacloud.uk/cpctrain/login?app=CPCDA"
     gov_gateway_return_url     = "https://tars-${var.environment}-cpc-internet.dvsa.tars.dev-dvsacloud.uk/cpctrain/login?app=CPCER"
     gov_gateway_issuer         = "https://mock-gov-gateway-${var.environment}-public.dvsa.tars.dev-dvsacloud.uk"
   }
