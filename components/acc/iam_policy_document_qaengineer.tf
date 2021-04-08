3e5fc6e4311ac7acc1f32053bfe6d3823d4926c5
tribbickm <michael.tribbick@bjss.com>
2021-04-08 15:25:37 +0100

    tsci_5419_metrics_post

--- components/acc/iam_policy_document_qaengineer.tf
+++ components/acc/iam_policy_document_qaengineer.tf
@@ -10,6 +10,10 @@ data "aws_iam_policy_document" "qaengineer" {
       "ec2:StopInstances",
       "ec2:DescribeInstances",
       "ec2:TerminateInstances",
+      "autoscaling:Describe*",
+      "cloudwatch:*",
+      "logs:*",
+      "sns:*",
     ]
 
     resources = [
