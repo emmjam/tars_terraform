ccc613625325c54fd1808e9965abc267b138b798
tribbickm <michael.tribbick@bjss.com>
2021-05-12 10:08:11 +0100

    cloudfront_dns_trust_advisor_change

--- components/base/route53_record_holding_pages.tf
+++ components/base/route53_record_holding_pages.tf
@@ -12,3 +12,44 @@ resource "aws_route53_record" "holding_pages" {
   }
 }
 
+resource "aws_route53_record" "holding_pages_findnearest" {
+
+  name = format("%s-%s", "findnearest", var.environment)
+
+  zone_id = data.terraform_remote_state.acc.outputs.public_domain_name_zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.holding_pages.domain_name
+    zone_id                = aws_cloudfront_distribution.holding_pages.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "holding_pages_dsa" {
+
+  name = format("%s-%s", "dsa", var.environment)
+
+  zone_id = data.terraform_remote_state.acc.outputs.public_domain_name_zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.holding_pages.domain_name
+    zone_id                = aws_cloudfront_distribution.holding_pages.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "holding_pages_driver_practical" {
+
+  name = format("%s-%s", "driver-practical", var.environment)
+
+  zone_id = data.terraform_remote_state.acc.outputs.public_domain_name_zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.holding_pages.domain_name
+    zone_id                = aws_cloudfront_distribution.holding_pages.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
