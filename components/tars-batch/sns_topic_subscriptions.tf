5ba9bb592b7653048277f82a4605c845ee447e53
Christopher Swanson <christopher.swanson@bjss.com>
2021-05-19 09:38:29 +0100

    ops-3570 remove opsgenie subscription from non prod alerts

--- components/tars-batch/sns_topic_subscriptions.tf
+++ components/tars-batch/sns_topic_subscriptions.tf
@@ -1,4 +1,5 @@
 resource "aws_sns_topic_subscription" "opsgenie" {
+  count                  = var.environment == "prod" ? 1 : 0
   topic_arn              = data.terraform_remote_state.base.outputs.sns_alerts_arn
   protocol               = "https"
   endpoint               = var.opsgenie_endpoint
--- components/tars-messaging/sns_topic_subscriptions.tf
+++ components/tars-messaging/sns_topic_subscriptions.tf
@@ -1,6 +1,6 @@
 resource "aws_sns_topic_subscription" "wms_unhealthy_hosts_opsgenie" {
+  count                  = var.environment == "prod" ? 1 : 0
   topic_arn              = data.terraform_remote_state.base.outputs.sns_alerts_arn
-  count                  = var.wms_host_alarm_count
   protocol               = "https"
   endpoint               = var.opsgenie_endpoint
   endpoint_auto_confirms = "true"
