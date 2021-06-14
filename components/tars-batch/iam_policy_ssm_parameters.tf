52217b2a811988fd0314f676c95fd96c7c6e843b
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-06-14 10:22:33 +0100

    Update iam_policy_ssm_parameters.tf

--- components/tars-batch/iam_policy_ssm_parameters.tf
+++ components/tars-batch/iam_policy_ssm_parameters.tf
@@ -12,7 +12,6 @@ data "aws_iam_policy_document" "ssm_parameters" {
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-TransferTheory*",
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-DocumentGeneration*",
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-ProcessUnknownCardAuthorisationsJob*",
-      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-ProcessUnknownCardAuthorisationsJob*",
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/all/*",
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/batch/*",
     ]
