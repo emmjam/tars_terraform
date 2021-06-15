e44d7b9ba5ab093d0841f95f8e64cb8a8f1ab51c
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-06-15 09:19:10 +0100

    Update iam_role_policy_attachments_tars_back.tf

--- components/tars-core/iam_role_policy_attachments_tars_back.tf
+++ components/tars-core/iam_role_policy_attachments_tars_back.tf
@@ -10,7 +10,7 @@ resource "aws_iam_role_policy_attachment" "tars_back_cloudwatch_tars_core" {
 
 resource "aws_iam_role_policy_attachment" "ssm_parameters_back" {
   role       = module.tars_back.iam_role_name
-  policy_arn = aws_iam_policy.ssm_parameters.arn
+  policy_arn = aws_iam_policy.ssm_parameters_back.arn
 }
 
 resource "aws_iam_role_policy_attachment" "oom_logs_s3_bucket" {
