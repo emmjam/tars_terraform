39fca7015e04563ca54482051b48b9689bb3c59e
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-06-15 09:13:59 +0100

    Update iam_policy_document_ssm_parameters.tf

--- components/tars-core/iam_policy_document_ssm_parameters.tf
+++ components/tars-core/iam_policy_document_ssm_parameters.tf
@@ -1,5 +1,5 @@
 # Policy to get  SSM Parameters
-data "aws_iam_policy_document" "ssm_parameters" {
+data "aws_iam_policy_document" "ssm_parameters_front" {
   statement {
     sid = "AllowGetSSMPars"
     actions = [
@@ -9,13 +9,36 @@ data "aws_iam_policy_document" "ssm_parameters" {
     resources = [
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-epdq-*",
       "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/all/*",
-      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/core/*",
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/front/*",
     ]
   }
 }
 
-resource "aws_iam_policy" "ssm_parameters" {
+resource "aws_iam_policy" "ssm_parameters_front" {
   name        = "${local.csi}-ssm_parameters"
-  description = "IAM policy for ${local.csi}-ssm_parameters"
-  policy      = data.aws_iam_policy_document.ssm_parameters.json
+  description = "IAM policy for ${local.csi}-ssm_parameters_front"
+  policy      = data.aws_iam_policy_document.ssm_parameters_front.json
+}
+
+
+# Policy to get  SSM Parameters
+data "aws_iam_policy_document" "ssm_parameters_back" {
+  statement {
+    sid = "AllowGetSSMPars"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/tars-${var.environment}-epdq-*",
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/all/*",
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/tars/back/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ssm_parameters_back" {
+  name        = "${local.csi}-ssm_parameters"
+  description = "IAM policy for ${local.csi}-ssm_parameters_back"
+  policy      = data.aws_iam_policy_document.ssm_parameters_back.json
 }
