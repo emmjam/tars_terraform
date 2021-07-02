d70e2d472eb1f55b7d8184be387fa712e583e8d4
Arthur Coombes <arthur.coombes@bjss.com>
2021-07-02 10:30:29 +0100

    Needs to Assume role in Production not nonprod

--- components/mgmt/data_iam_policy_document_rsis_tif_access.tf
+++ components/mgmt/data_iam_policy_document_rsis_tif_access.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "assume_rsis_tif_access_role" {
     ]
 
     resources = [
-      "arn:aws:iam::652856684323:role/DVSATifAccess"
+      "arn:aws:iam::246976497890:role/DVSATifAccess"
     ]
   }
 }
