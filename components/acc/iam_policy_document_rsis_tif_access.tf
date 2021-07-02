5211ea7fb914ef8125bc83945a643984fc599832
Arthur Coombes <arthur.coombes@bjss.com>
2021-07-02 13:50:34 +0100

    Grant access to contents of buckets as well as the buckets themselves

--- components/acc/iam_policy_document_rsis_tif_access.tf
+++ components/acc/iam_policy_document_rsis_tif_access.tf
@@ -11,7 +11,9 @@ data "aws_iam_policy_document" "rsis_tif_access" {
 
     resources = [
       "arn:aws:s3:::tars-246976497890-eu-west-1-prod-tars-batch-rsistiffbucket",
+      "arn:aws:s3:::tars-246976497890-eu-west-1-prod-tars-batch-rsistiffbucket/*",
       "arn:aws:s3:::tars-246976497890-eu-west-1-prod-tars-batch-rsistiffbucket2",
+      "arn:aws:s3:::tars-246976497890-eu-west-1-prod-tars-batch-rsistiffbucket2/*",
     ]
   }
 }
