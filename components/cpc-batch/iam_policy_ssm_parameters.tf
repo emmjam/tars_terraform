0b0bc67dc228f6b9fb7760f142213e6d9761db7e
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-09 09:44:57 +0000

    Add new file

---
+++ components/cpc-batch/iam_policy_ssm_parameters.tf
@@ -0,0 +1,21 @@
+# Policy to get  SSM Parameters
+data "aws_iam_policy_document" "ssm_parameters" {
+  statement {
+    sid = "AllowGetSSMPars"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/cpc/all/*",
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/cpc/batch/*",
+      "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/cpc/back/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ssm_parameters" {
+  name        = "${local.csi}-ssm_parameters"
+  description = "IAM policy for ${local.csi}-ssm_parameters"
+  policy      = data.aws_iam_policy_document.ssm_parameters.json
+}
