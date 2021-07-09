e3537ae5d9a82727b6451d959d9b44f05a3df378
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-09 09:46:06 +0000

    Update iam_role_policy_attachments_cpc_batch.tf

--- components/cpc-batch/iam_role_policy_attachments_cpc_batch.tf
+++ components/cpc-batch/iam_role_policy_attachments_cpc_batch.tf
@@ -8,3 +8,8 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_tars_core" {
   policy_arn = data.terraform_remote_state.base.outputs.iam_policy_cloudwatch_arn
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_parameters_batch" {
+  role       = module.cpc_batch.iam_role_name
+  policy_arn = aws_iam_policy.ssm_parameters.arn
+}
+
