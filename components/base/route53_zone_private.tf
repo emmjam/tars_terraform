d5cbe37c16a20d056b27f1145ec1fb2e833adc5c
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-27 10:55:56 +0000

    Update route53_zone_private.tf

--- components/base/route53_zone_private.tf
+++ components/base/route53_zone_private.tf
@@ -2,7 +2,7 @@
 # Keep it generic so it's the same across all VPC's/envs
 resource "aws_route53_zone" "vpc" {
   name    = local.vpc_domain_name
-  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name} hosted zone"
+  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name_aws} hosted zone"
 
   vpc {
     vpc_region = var.aws_region
@@ -15,3 +15,19 @@ resource "aws_route53_zone" "vpc" {
   }
 }
 
+# Create the private R53 zone for the tars apps
+# Keep it generic so it's the same across all VPC's/envs
+resource "aws_route53_zone" "vpc_private" {
+  name    = local.vpc_domain_name
+  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name} hosted zone"
+
+  vpc {
+    vpc_region = var.aws_region
+    vpc_id     = aws_vpc.vpc.id
+  }
+
+  # See https://github.com/terraform-providers/terraform-provider-aws/issues/7805
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+}
