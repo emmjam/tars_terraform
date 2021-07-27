6e84f5e6db4a00180d64ff3ec92ab4dc124fdac0
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-27 15:16:48 +0000

    Update route53_records_private.tf

--- components/base/route53_records_private.tf
+++ components/base/route53_records_private.tf
@@ -22,3 +22,26 @@ resource "aws_route53_record" "awsmq" {
   ]
 }
 
+# R53 record for the AWS MQ endpoint
+resource "aws_route53_record" "awsmq-priv" {
+  count = var.aws_mq_deployment_mode == "SINGLE_INSTANCE" ? 1 : 2
+
+  name = format("%s-%s-%s", var.project, "awsmq", count.index + 1)
+
+  zone_id = aws_route53_zone.vpc-private.zone_id
+  type    = "CNAME"
+  ttl     = 60
+
+  # Terraform's support for list and map data handling pre 0.12.* 
+  # is terrible. We manually construct the DNS entry required until
+  # we can either upgrade to 0.12 for better splat/lookup support
+  # or we decide it's safe to use the IPs provided by aws_mq_broker
+  records = [
+    format(
+      "%s-%d.mq.%s.amazonaws.com",
+      aws_mq_broker.tars-awsmq.id,
+      count.index + 1,
+      var.aws_region,
+    ),
+  ]
+}
