f81cb03c26abe3a58481fa4319a7e2508751b3a3
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-27 12:02:38 +0000

    Update route53_public_zone.tf

--- components/base/route53_public_zone.tf
+++ components/base/route53_public_zone.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "public_domain" {
-  name          = var.private_domain_name
+  name          = local.vpc_domain_name
   comment       = "The environemnt level Public Hosted Zone used for ACM"
   force_destroy = "false"
 }
