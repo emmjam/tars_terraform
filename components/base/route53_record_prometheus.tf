04dc9832740aa97ab791b0e0273633490500d047
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-28 14:35:35 +0000

    Update route53_record_prometheus.tf

--- components/base/route53_record_prometheus.tf
+++ components/base/route53_record_prometheus.tf
@@ -1,15 +1,14 @@
-resource "aws_route53_record" "prometheus-private" {
-  name = format("%s.%s", "prometheus", var.component)
+#resource "aws_route53_record" "prometheus-private" {
+#  name = format("%s.%s", "prometheus", var.component)
+#  zone_id = aws_route53_zone.vpc.zone_id
+#  type    = "A"
 
-  zone_id = aws_route53_zone.vpc.zone_id
-  type    = "A"
-
-  alias {
-    name                   = aws_alb.prometheus-private.dns_name
-    zone_id                = aws_alb.prometheus-private.zone_id
-    evaluate_target_health = true
-  }
-}
+#  alias {
+#    name                   = aws_alb.prometheus-private.dns_name
+#    zone_id                = aws_alb.prometheus-private.zone_id
+#    evaluate_target_health = true
+#  }
+#}
 
 resource "aws_route53_record" "prometheus" {
   name = format("%s.%s", "prometheus", var.component)
