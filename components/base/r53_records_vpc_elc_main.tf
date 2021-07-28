c4206b9d3819465914a38c0067733fda32c98335
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-28 14:34:44 +0000

    Update r53_records_vpc_elc_main.tf

--- components/base/r53_records_vpc_elc_main.tf
+++ components/base/r53_records_vpc_elc_main.tf
@@ -1,14 +1,14 @@
 # Record 'name' left as "-web" for backwards compatability.
-resource "aws_route53_record" "elc-main-CNAME" {
-  zone_id = aws_route53_zone.vpc.zone_id
-  name    = "elasticache.cfg.main"
-  type    = "CNAME"
-  ttl     = "30"
+#resource "aws_route53_record" "elc-main-CNAME" {
+#  zone_id = aws_route53_zone.vpc.zone_id
+#  name    = "elasticache.cfg.main"
+#  type    = "CNAME"
+#  ttl     = "30"
 
-  records = [
-    aws_elasticache_cluster.ec.cluster_address,
-  ]
-}
+#  records = [
+#    aws_elasticache_cluster.ec.cluster_address,
+#  ]
+#}
 
 # Record 'name' left as "-web" for backwards compatability.
 #dvsa-cloud private domain
