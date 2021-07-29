ad129e9fdc0b6b79c99428fdd76f3c0bd00755d8
Raman Kapoor <kapoorr@LPT5692.bjss.co.uk>
2021-07-29 12:22:35 +0100

    update tars db free storage alarm

--- components/tars-core/rds_cw_metric_freestoragespace_alarm.tf
+++ components/tars-core/rds_cw_metric_freestoragespace_alarm.tf
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_freestoragespace_average" {
 
   period               = "300"
   statistic            = "Average"
-  threshold            = "5000000000" # 5GiB
+  threshold            = "${var.tars_rds_allocated_storage}00000000" # 10% of allocated storage
   unit                 = "Bytes"
   alarm_description    = "Alarm when the Average RDS Free Storage Space is less than or equal to 5GB for a period of 5 minutes."
   actions_enabled      = var.rds_cw_metric_freestoragespace_alarm_action_enabled
