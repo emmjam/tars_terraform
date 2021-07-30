1fe9970c3f12d300e6ac242b09985865f5ee7c54
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-07-30 14:54:12 +0000

    Update data_iam_policy_jenkins_worker.tf

--- components/ctrl/data_iam_policy_jenkins_worker.tf
+++ components/ctrl/data_iam_policy_jenkins_worker.tf
@@ -13,6 +13,19 @@ data "aws_iam_policy_document" "jenkins_worker_iam" {
   }
 
   statement {
+    sid    = ""
+    effect = "Allow"
+
+    actions = [
+      "route53:AssociateVPCWithHostedZone",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
     sid = "DenyCreateUser"
 
     effect = "Deny"
