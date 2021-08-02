82e011bce031d4287fadc05380382a38309b8fbf
Christopher Swanson <christopher.swanson@bjss.com>
2021-08-02 08:05:38 +0100

    ops-3770 rebase

--- modules/cwles-vpcflowlog/vpc_flow_logs.tf
+++ modules/cwles-vpcflowlog/vpc_flow_logs.tf
@@ -1,13 +1,8 @@
 # The VPC Flow Log stream to send VPC network data
 # the a CloudWatch Logs log group
 resource "aws_flow_log" "main" {
-  log_group_name = format(
-    "%s-%s-%s-%s",
-    var.project,
-    var.environment,
-    var.component,
-    "vpc-flow-logs",
-  )
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  log_destination_type = "cloud-watch-logs"
 
   iam_role_arn = aws_iam_role.vpc_flow_logs.arn
   vpc_id       = var.vpc_id
