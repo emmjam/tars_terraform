0acda3a946d26eb9f4ea1c14001c7019a96af4ed
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-12 07:31:49 +0000

    Update route53_zone.tf

--- components/acc/route53_zone.tf
+++ components/acc/route53_zone.tf
@@ -8,8 +8,16 @@ resource "aws_route53_zone" "public_domain" {
 resource "aws_route53_zone" "public_domain" {
   count         = var.public_domain_name == "" ? "0" : "1"
   name          = var.public_domain_name
-  comment       = "The account level Public Hosted Zone"
-  force_destroy = "false"
+  comment = "Private ${var.component} ${var.environment} ${var.private_domain_name} hosted zone"
+  vpc {
+    vpc_region = var.aws_region
+    vpc_id     = aws_vpc.vpc.id
+  }
+
+  # See https://github.com/terraform-providers/terraform-provider-aws/issues/7805
+  lifecycle {
+    ignore_changes = [vpc]
+  }
 }
 
 resource "aws_route53_zone" "public_domain_root" {
