f41dbe9888007c64a3ad1206430d10bdef41cd37
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-13 11:31:41 +0000

    Update route53_public_zone.tf

--- components/base/route53_public_zone.tf
+++ components/base/route53_public_zone.tf
@@ -4,10 +4,3 @@ resource "aws_route53_zone" "public_domain" {
   force_destroy = "false"
 }
 
-resource "aws_route53_record" "public_domain" {
-  zone_id = data.terraform_remote_state.ctrl.outputs.private_r53_zone
-  name    = local.vpc_domain_name
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.public_domain.*.name_servers
-}
