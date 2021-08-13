1b33ce0f681fa3971fd0837d16bae42099db0e5c
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-13 11:10:00 +0000

    Update route53_public_zone.tf

--- components/base/route53_public_zone.tf
+++ components/base/route53_public_zone.tf
@@ -3,3 +3,11 @@ resource "aws_route53_zone" "public_domain" {
   comment       = "The environment level Public Hosted Zone used for ACM"
   force_destroy = "false"
 }
+
+resource "aws_route53_record" "public_domain" {
+  zone_id = aws_route53_zone.public_domain.zone_id
+  name    = local.vpc_domain_name
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.public_domain.name_servers
+}
