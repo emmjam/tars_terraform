dbd0315b248f155f9f223edeca307afaa4dce1df
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-16 13:33:22 +0000

    Update route53_public_zone.tf

--- components/base/route53_public_zone.tf
+++ components/base/route53_public_zone.tf
@@ -7,7 +7,7 @@ resource "aws_route53_zone" "public_domain" {
 resource "aws_route53_record" "public_domain_nonprod" {
   count   = var.aws_account_alias == "tarsnonprod" ? 1 : 0
   zone_id = data.terraform_remote_state.acc.outputs.public_domain_root_zone_id
-  name    = local.vpc_domain_name
+  name    = local.new_vpc_domain_name
   type    = "NS"
   ttl     = "30"
   records = aws_route53_zone.public_domain.name_servers
