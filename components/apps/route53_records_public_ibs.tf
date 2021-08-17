4180d3480538a257b803b9f1ba3c899659d16067
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-17 14:30:07 +0000

    Update route53_records_public_ibs.tf

--- components/apps/route53_records_public_ibs.tf
+++ components/apps/route53_records_public_ibs.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "ibs-front" {
   }
 }
 
-resource "aws_route53_record" "ibs-front_priv" {
+resource "aws_route53_record" "ibs-front_private" {
   name    = format("%s-%s-%s", "ibs", var.environment, "public")
   zone_id = data.terraform_remote_state.ctrl.outputs.private_r53_zone[0]
   type    = "A"
