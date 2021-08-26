1d089ff5137b89032fadb67efba14e13872833b1
Michelle Pike <mailto:michelle.pike@bjss.com>
2021-08-26 13:00:12 +0000

    Update r53_zone_association_ctrl.tf

--- components/base/r53_zone_association_ctrl.tf
+++ components/base/r53_zone_association_ctrl.tf
@@ -2,8 +2,3 @@ resource "aws_route53_zone_association" "vpc_ctrl" {
   zone_id = aws_route53_zone.vpc.zone_id
   vpc_id  = data.terraform_remote_state.ctrl.outputs.vpc_id
 }
-
-resource "aws_route53_zone_association" "vpc_private" {
-  zone_id = aws_route53_zone.vpc.zone_id
-  vpc_id  = data.terraform_remote_state.ctrl.outputs.vpc_id
-}
