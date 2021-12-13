b8d882e5939ba2ed68ff9cfd62901a8d32b1cc0d
Saul Kashani <saul.kashani@bjss.com>
2021-12-13 11:45:01 +0000

    CHG0045015 mgmt listener nexus chg

--- components/mgmt/alb_listener_private_https.tf
+++ components/mgmt/alb_listener_private_https.tf
@@ -3,7 +3,7 @@ resource "aws_alb_listener" "private_https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.mgmt_mgmt_tars_dvsa_aws.arn
+  certificate_arn   = data.aws_acm_certificate.tars_dvsacloud_uk.arn
 
   default_action {
     type             = "forward"
--- components/mgmt/nexus_certificate.tf
+++
@@ -1,8 +0,0 @@
-data "aws_acm_certificate" "mgmt_mgmt_tars_dvsa_aws" {
-  domain = var.nexus_domain_name
-
-  statuses = [
-    "ISSUED",
-  ]
-}
-
