fc5f3e5620b44ad6671bef239e1e1d6cf341cd01
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-10 11:29:45 +0000

    Update sqs_email_dlq.tf

--- components/base/sqs_email_dlq.tf
+++ components/base/sqs_email_dlq.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "govnotify_dlq" {
-  name                       = "${local.csi}_govnotify_dlq.fifo"
+  name = "${local.csi}-govnotify-dlq.fifo"
 
   tags = merge(
     local.default_tags,
     {
-      "Name" = "${local.csi}/govnotify_dlq"
+      "Name" = "${local.csi}/govnotify-dlq"
     },
   )
 }
