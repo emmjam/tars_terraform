f54dbcc44e285c880cb3368ca308c190b07fad3c
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-10 09:54:16 +0000

    Update sqs_policy_govnotify_send.tf

--- components/tars-batch/sqs_policy_govnotify_send.tf
+++ components/tars-batch/sqs_policy_govnotify_send.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue_policy" "send_gov_notify" {
-  queue_url = aws_sqs_queue.send_gov_notify.id
+  queue_url = data.terraform_remote_state.tars-core.outputs.sqs_send_govnotify_id
 
   policy = <<POLICY
 {
@@ -7,13 +7,35 @@ resource "aws_sqs_queue_policy" "send_gov_notify" {
   "Id": "sqspolicy",
   "Statement":  [
     {
-      "Sid": "Stmt1641547592020",
+      "Sid": "AllowGNSendSQS",
       "Effect": "Allow",
       "Principal": {
         "AWS": "${module.tars_batch.iam_role_name}"
       },
       "Action": "sqs:*",
       "Resource": "${data.terraform_remote_state.tars-core.outputs.sqs_send_govnotify_arn}"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sqs_queue_policy" "results_gov_notify" {
+  queue_url = data.terraform_remote_state.tars-core.outputs.sqs_results_govnotify_id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement":  [
+    {
+      "Sid": "AllowGNResultsSQS",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${module.tars_batch.iam_role_name}"
+      },
+      "Action": "sqs:*",
+      "Resource": "${data.terraform_remote_state.tars-core.outputs.sqs_results_govnotify_arn}"
     }
   ]
 }
