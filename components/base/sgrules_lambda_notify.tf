b33c65bf49d872e090a2745b7ac6b908b2986a63
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-10 09:22:48 +0000

    Update sgrules_lambda_notify.tf

--- components/base/sgrules_lambda_notify.tf
+++ components/base/sgrules_lambda_notify.tf
@@ -13,6 +13,6 @@ resource "aws_security_group_rule" "squidnat_egress_govnotify" {
   protocol                 = "tcp"
   from_port                = "443"
   to_port                  = "443"
-  source_security_group_id = aws_security_group.vpc_endpoint.id
+  source_security_group_id = aws_security_group.vpc_endpoints.id
   security_group_id        = module.lambda_notify.security_group_id
 }
