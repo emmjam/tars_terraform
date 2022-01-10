9349fcb3cf7d92aed9f773569e380257a0d67674
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-10 14:34:46 +0000

    Update key_policy_iam_access.tf

--- components/base/key_policy_iam_access.tf
+++ components/base/key_policy_iam_access.tf
@@ -15,5 +15,37 @@ data "aws_iam_policy_document" "kms_root_managed" {
     resources = [
       "*"
     ]
+  },
+  {
+    sid    = "Enable IAM policies"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.aws_account_id}:root"]
+    }
+
+    actions =  [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:CreateGrant",
+                "kms:DescribeKey"
+            ]
+
+    resources = [
+      "*"
+    ]
+    condition = {
+                test     = "StringEquals"
+                variable = "kms:CallerAccount"
+                values = [ "652856684323" ]
+            }
+    condition = {
+                test     = "StringEquals"
+                variable = "kms:ViaService"
+                values = [ "sqs.eu-west-1.amazonaws.com" ]
+            }
   }
 }
