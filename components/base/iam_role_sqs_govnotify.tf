c349cd9c83d410acfdb5b5f77bf54874ad9a2607
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-11 10:18:44 +0000

    Add new file

---
+++ components/base/iam_role_sqs_govnotify.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "sqs_govnotify" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["sqs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "sqs_govnotify" {
+  name = format(
+    "%s-%s-%s-%s",
+    var.project,
+    var.environment,
+    var.component,
+    "db-reports"
+  )
+
+  assume_role_policy = data.aws_iam_policy_document.sqs_govnotify.json
+}
+
+resource "aws_iam_role_policy" "sqs_govnotify" {
+  name = format(
+    "%s-%s-%s-%s",
+    var.project,
+    var.environment,
+    var.component,
+    "sqs_govnotify"
+  )
+
+  role   = aws_iam_role.sqs_govnotify.id
+  policy = data.aws_iam_policy_document.sqs_govnotify.json
+}
