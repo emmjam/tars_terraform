e13c9789eac694cc3b3453212c8fca25790217c8
Michelle Pike <mailto:michelle.pike@bjss.com>
2022-01-11 12:26:26 +0000

    Update outputs.tf

--- components/base/outputs.tf
+++ components/base/outputs.tf
@@ -186,3 +186,7 @@ output "sqs_send_govnotify_id" {
 output "sqs_results_govnotify_id" {
   value = aws_sqs_queue.results_gov_notify.id
 }
+
+output "lambda_notify_role_arn" {
+  valure = module.lambda_notify.role_arn
+}
