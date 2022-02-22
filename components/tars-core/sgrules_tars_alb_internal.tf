5dc67a26cc132dce56ea751fd1f37c210390fdba
daviesi <ian.davies@bjss.com>
2022-02-22 15:51:42 +0000

    added SG from public alb to internal alb

--- components/tars-core/sgrules_tars_alb_internal.tf
+++ components/tars-core/sgrules_tars_alb_internal.tf
@@ -8,17 +8,6 @@ resource "aws_security_group_rule" "tars_alb_internal_ingress_apache" {
   source_security_group_id = module.apache.security_group_id
 }
 
-resource "aws_security_group_rule" "tars_alb_internal_public_ingress_alb" {
-  # Will replace Apache rule above  - IanD
-  description              = "Allow TCP/80 from Public ALB"
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.tars-alb-internal.id
-  source_security_group_id = module.apache.security_group_id
-}
-
 resource "aws_security_group_rule" "tars_alb_public_egress_tars_front_port_8443" {
   description              = "Allow TCP/8443 to tars core frontend"
   type                     = "egress"
@@ -39,3 +28,13 @@ resource "aws_security_group_rule" "tars_alb_public_egress_irdt_front_port_7443_
   source_security_group_id = module.tars_front.security_group_id
 }
 
+resource "aws_security_group_rule" "tars_alb_public_ingress_alb" {
+  # Will replace the apache ingress above -- IanD
+  description              = "Allow TCP/80 from Public ALB"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.apache_alb_public
+  source_security_group_id = module.apache.security_group_id
+}
