2c1e55cb1b8cbe2dda6bead6e3d5076a13c7464b
daviesi <ian.davies@bjss.com>
2022-02-23 15:52:14 +0000

    LB rules test

--- components/tars-core/alb_listener_rule_public.tf
+++ components/tars-core/alb_listener_rule_public.tf
@@ -194,6 +194,27 @@ resource "aws_lb_listener_rule" "rewrite_dsaweb_7" {
   }
 }
 
+resource "aws_lb_listener_rule" "irdt_proxy" {
+  #  ProxyPassReverse   "http://irdt-internal.opsdev.nonprod.tars.dev-dvsacloud.uk/DSAWeb"
+  listener_arn = aws_alb_listener.apache-https-public.arn
+  priority     = "40"
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.irdt-frontend2-7443.arn
+  }
+
+  condition {
+    path_pattern {
+    values = ["/DSAWeb"]
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb_listener_rule" "rewrite_obsweb_1" {
   listener_arn = aws_alb_listener.apache-https-public.arn
   priority     = "45"
@@ -250,6 +271,27 @@ resource "aws_lb_listener_rule" "rewrite_obsweb_2" {
   }
 }
 
+resource "aws_lb_listener_rule" "obs_proxy" {
+  #  ProxyPassReverse   "http://obs-internal.opsdev.nonprod.tars.dev-dvsacloud.uk/obs-web"
+  listener_arn = aws_alb_listener.apache-https-public.arn
+  priority     = "55"
+
+  action {
+    type             = "forward"
+    target_group_arn = data.terraform_remote_state.apps.outputs.obs-tg-8080-arn
+  }
+
+  condition {
+    path_pattern {
+    values = ["/obs-web"]
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb_listener_rule" "rewrite_cpc_1" {
   listener_arn = aws_alb_listener.apache-https-public.arn
   priority     = "60"
@@ -298,6 +340,27 @@ resource "aws_lb_listener_rule" "rewrite_cpc_2" {
   condition {
     path_pattern {
     values = ["/delegated/"]
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener_rule" "cpc_proxy" {
+  #  ProxyPassReverse   "http://cpc-internal.opsdev.nonprod.tars.dev-dvsacloud.uk/cpcode"
+  listener_arn = aws_alb_listener.apache-https-public.arn
+  priority     = "70"
+
+  action {
+    type             = "forward"
+    target_group_arn = data.terraform_remote_state.apps.outputs.cpc-tg-9443-arn
+  }
+
+  condition {
+    path_pattern {
+    values = ["/cpctrain"]
     }
   }
 
