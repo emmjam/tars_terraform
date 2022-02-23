5b7f8b479726257d9d8d9cf1deb021b07dc18fee
daviesi <ian.davies@bjss.com>
2022-02-23 16:43:59 +0000

    LB rules test

--- components/cpc/alb_front_target_group_internal.tf
+++ components/cpc/alb_front_target_group_internal.tf
@@ -24,12 +24,11 @@ resource "aws_alb_target_group" "cpc-front-internal-9443" {
 resource "aws_alb_target_group" "cpc-front-internal2-9443" {
   name     = "${local.csi}-internal2-9443"
   port     = "9443"
-  protocol = "HTTPS"
+  protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.base.outputs.vpc_id
 
   health_check {
     path                = "/cpctrain/ping"
-    protocol            = "HTTPS"
     timeout             = 5
     interval            = 10
     healthy_threshold   = 3
@@ -42,3 +41,25 @@ resource "aws_alb_target_group" "cpc-front-internal2-9443" {
     enabled = true
   }
 }
+
+#resource "aws_alb_target_group" "cpc-front-internal2-9443" {
+# name     = "${local.csi}-internal2-9443"
+# port     = "9443"
+# protocol = "HTTPS"
+# vpc_id   = data.terraform_remote_state.base.outputs.vpc_id
+#
+# health_check {
+#   path                = "/cpctrain/ping"
+#   protocol            = "HTTPS"
+#   timeout             = 5
+#   interval            = 10
+#   healthy_threshold   = 3
+#   unhealthy_threshold = 3
+#   matcher             = 200
+# }
+#
+# stickiness {
+#   tpe    = "lb_cookie"
+#   enabled = true
+# }
+#}
