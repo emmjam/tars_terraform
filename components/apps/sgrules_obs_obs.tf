370895e521f4435b68fb8cac9414944c5a5b06a8
daviesi <ian.davies@bjss.com>
2022-02-28 13:17:50 +0000

    testing

--- components/apps/sgrules_obs_obs.tf
+++ components/apps/sgrules_obs_obs.tf
@@ -7,21 +7,22 @@ resource "aws_security_group_rule" "obs-ingress-private-alb-8080" {
   source_security_group_id = aws_security_group.private-alb.id
 }
 
+resource "aws_security_group_rule" "obs-ingress-public-alb-8080" {
+  # Removing Apache  -- IanD
+  description              = "Incomming from tars-core-apache public alb"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = module.obs.security_group_id
+  source_security_group_id = aws_security_group.private-alb.id
+}
+
 resource "aws_security_group_rule" "obs-egress-tars-back-8080" {
   type                     = "egress"
   protocol                 = "tcp"
   from_port                = "8080"
   to_port                  = "8080"
-  security_group_id        = module.obs.security_group_id
-  source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-backend-alb-sg-id
-}
-
-resource "aws_security_group_rule" "obs-test" {
-  type                     = "egress"
-  description = "iTEST"
-  protocol                 = "tcp"
-  from_port                = "8089"
-  to_port                  = "8089"
   security_group_id        = module.obs.security_group_id
   source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-backend-alb-sg-id
 }
