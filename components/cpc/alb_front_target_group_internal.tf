fcf48986b9cb7b0cdceb3ed75b471ae6d2561df0
daviesi <ian.davies@bjss.com>
2022-02-28 15:04:10 +0000

    alb target group to https

--- components/cpc/alb_front_target_group_internal.tf
+++ components/cpc/alb_front_target_group_internal.tf
@@ -24,11 +24,12 @@ resource "aws_alb_target_group" "cpc-front-internal-9443" {
 resource "aws_alb_target_group" "cpc-front-internal2-9443" {
   name     = "${local.csi}-internal2-9443"
   port     = "9443"
-  protocol = "HTTP"
+  protocol = "HTTPS"
   vpc_id   = data.terraform_remote_state.base.outputs.vpc_id
 
   health_check {
     path                = "/cpctrain/ping"
+    protocol            = "HTTPS"
     timeout             = 5
     interval            = 10
     healthy_threshold   = 3
