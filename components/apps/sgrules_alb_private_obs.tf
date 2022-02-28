fbc191ab1fc523652b123cd78b97529b5e4b0bc4
daviesi <ian.davies@bjss.com>
2022-02-28 16:35:30 +0000

    removed items associated with apache

--- components/apps/sgrules_alb_private_obs.tf
+++ components/apps/sgrules_alb_private_obs.tf
@@ -1,12 +1,3 @@
-resource "aws_security_group_rule" "private_alb_ingress_public" {
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = "80"
-  to_port                  = "80"
-  security_group_id        = aws_security_group.private-alb.id
-  source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-apache-sg-id
-}
-
 resource "aws_security_group_rule" "obs_alb_jmeter_egress_jenkinsnode_1099_1101" {
   type                     = "egress"
   protocol                 = "tcp"
