b932406166de6bdf184d0cfe568cb203d7e03775
daviesi <ian.davies@bjss.com>
2022-02-28 13:23:41 +0000

    testing

--- components/tars-core/outputs.tf
+++ components/tars-core/outputs.tf
@@ -19,6 +19,10 @@ output "tars-core-apache-sg-id" {
   value = module.apache.security_group_id
 }
 
+output "tars-core-public-alb-sg-id" {
+  value = aws_security_group.apache_alb_public.id
+}
+
 output "tars-core-alb-target-group-irdt-frontend" {
   value = aws_alb_target_group.irdt-frontend-7443.arn
 }
