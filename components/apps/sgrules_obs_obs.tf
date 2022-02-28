5913b7b3fe58ec98241f01803ef01142bec12f47
daviesi <ian.davies@bjss.com>
2022-02-28 13:11:01 +0000

    testing

--- components/apps/sgrules_obs_obs.tf
+++ components/apps/sgrules_obs_obs.tf
@@ -16,6 +16,16 @@ resource "aws_security_group_rule" "obs-egress-tars-back-8080" {
   source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-backend-alb-sg-id
 }
 
+resource "aws_security_group_rule" "obs-egress-tars-back-8080" {
+  type                     = "egress"
+  description = "iTEST"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = module.obs.security_group_id
+  source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-backend-alb-sg-id
+}
+
 resource "aws_security_group_rule" "obs_egress_oracle_db" {
   type                     = "egress"
   protocol                 = "tcp"
