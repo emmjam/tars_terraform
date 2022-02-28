a05890d623a68f5490993873eddd70546b7c2093
daviesi <ian.davies@bjss.com>
2022-02-28 16:32:16 +0000

    removed items associated with apache

--- components/cpc/sgrules_cpc_front_internal_alb.tf
+++ components/cpc/sgrules_cpc_front_internal_alb.tf
@@ -1,12 +1,3 @@
-resource "aws_security_group_rule" "cpc-front-alb_ingress_public-80" {
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = "80"
-  to_port                  = "80"
-  security_group_id        = aws_security_group.cpc-front-internal-alb.id
-  source_security_group_id = data.terraform_remote_state.tars-core.outputs.tars-core-apache-sg-id
-}
-
 resource "aws_security_group_rule" "cpc-front-alb_egress_cpc-front-9443" {
   type                     = "egress"
   protocol                 = "tcp"
