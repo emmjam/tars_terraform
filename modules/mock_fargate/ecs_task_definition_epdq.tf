d9e8ee38d17078dc4270929cbd211128cfb8205f
tribbickm <michael.tribbick@bjss.com>
2022-04-27 09:43:31 +0100

    mock_epdq_async_callback

--- modules/mock_fargate/ecs_task_definition_epdq.tf
+++ modules/mock_fargate/ecs_task_definition_epdq.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "epdq" {
         epdq_stream_prefix   = "${local.csi}-ecs-epdq"
         epdq_name            = local.epdq_name
         epdq_response_domain = "https://mock-epdq-${var.environment}-public.dvsa.tars.dev-dvsacloud.uk"
-        async_callback_url   = "http://tars-core-backend.${var.environment}.tars.dvsa.aws:8080/DSAPostPaymentConfirmationService/confirm"
+        async_callback_url   = "http://tars-core-backend.${var.environment}.nonprod.tars.dev-dvsacloud.uk:8080/DSAPostPaymentConfirmationService/confirm"
       })
 
   requires_compatibilities = tolist(["FARGATE"])
