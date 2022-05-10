cb77d474029e3363d1ebb6a7b6bc706547600af0
Aamir Soni <aamir.soni@bjss.com>
2022-05-10 14:37:22 +0100

    remove policy

--- modules/cross-kms/kms_key.tf
+++ modules/cross-kms/kms_key.tf
@@ -9,7 +9,6 @@ resource "aws_kms_key" "main" {
   )
 
   deletion_window_in_days = var.deletion_window
-  policy                  = data.aws_iam_policy_document.key2.json
 
   tags = merge(
     var.default_tags,
