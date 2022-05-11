1dec7bd889da836c615ff2dc8ae9cd96284746ee
Aamir Soni <aamir.soni@bjss.com>
2022-05-11 15:49:25 +0100

    remove unused file and change grantee principle

--- modules/cross-kms/grant_kms.tf
+++ modules/cross-kms/grant_kms.tf
@@ -6,6 +6,6 @@ resource "aws_iam_role" "autoscaling_grant_access" {
 resource "aws_kms_grant" "autoscaling_grant_access" {
   name              = "platform-ops-grant-access-autoscaling"
   key_id            = aws_kms_key.main.key_id
-  grantee_principal = aws_iam_role.autoscaling_grant_access.arn
+  grantee_principal = "arn:aws:iam::645711882182:role/platform-ops-grant-access-autoscaling"
   operations        = ["Encrypt", "Decrypt", "ReEncryptFrom","GenerateDataKey","GenerateDataKeyWithoutPlaintext","DescribeKey","CreateGrant"]
 }
