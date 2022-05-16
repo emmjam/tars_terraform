b484791f7c8d986e51a5899f60331649d905a10f
Aamir Soni <aamir.soni@bjss.com>
2022-05-16 13:18:13 +0100

    add condition back in

--- modules/cross-kms/data_iam_kms_key_policy.tf
+++ modules/cross-kms/data_iam_kms_key_policy.tf
@@ -119,6 +119,11 @@ data "aws_iam_policy_document" "key" {
       "*",
     ]
     
+    condition {
+      test = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values = ["true"]
+    }
   }
 
 }
