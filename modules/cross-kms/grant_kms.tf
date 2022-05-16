c7d1074dd3339782af7422490bd40adfb66482ac
Aamir Soni <aamir.soni@bjss.com>
2022-05-16 12:11:58 +0100

    add grant for mgmt

--- modules/cross-kms/grant_kms.tf
+++ modules/cross-kms/grant_kms.tf
@@ -1,5 +1,5 @@
 resource "aws_kms_grant" "autoscaling_grant_access" {
-  count = contains(["prod","nonprod"],var.environment) ? 1 : 0
+  count = contains(["prod","nonprod","mgmt"],var.environment) ? 1 : 0
   name              = "platform-ops-grant-access-autoscaling"
   key_id            = "arn:aws:kms:eu-west-1:645711882182:key/a32e7e37-ff98-4904-bc14-a1d3256755c8"
   grantee_principal = "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling" 
