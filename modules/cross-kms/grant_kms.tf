ed7782039d75c87570972189ad43542c2d3d6d26
Aamir Soni <aamir.soni@bjss.com>
2022-05-16 12:49:37 +0100

    add kms key

--- modules/cross-kms/grant_kms.tf
+++ modules/cross-kms/grant_kms.tf
@@ -1,7 +1,7 @@
 resource "aws_kms_grant" "autoscaling_grant_access" {
   count = contains(["live","nonprod","mgmt"],var.environment) ? 1 : 0
   name              = "platform-ops-grant-access-autoscaling"
-  key_id            = "arn:aws:kms:eu-west-1:645711882182:key/a32e7e37-ff98-4904-bc14-a1d3256755c8"
+  key_id            = aws_kms_key.main[0].arn
   grantee_principal = "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling" 
   operations        = ["Encrypt", "Decrypt", "ReEncryptFrom","GenerateDataKey","GenerateDataKeyWithoutPlaintext","DescribeKey","CreateGrant"]
 }
