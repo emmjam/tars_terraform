3aab417f0f562adaf75862f3d750b2d0f378b5c7
tribbickm <michael.tribbick@bjss.com>
2022-05-17 16:09:29 +0100

    tsci-7077-sqs-rename

--- components/base/sqs_letters.tf
+++ components/base/sqs_letters.tf
@@ -1,17 +1,17 @@
 resource "aws_sqs_queue" "printgovnotify" {
-  name       = "${local.csi}-PrintGovNotify.fifo"
+  name       = "${local.csi}-Queue-PrintGovNotify.fifo"
   fifo_queue = true
 
   tags = merge(
     local.default_tags,
     {
-      "Name" = "${local.csi}/PrintGovNotify"
+      "Name" = "${local.csi}/Queue-PrintGovNotify"
     },
   )
 }
 
 resource "aws_ssm_parameter" "sqs_printgovnotify" {
-  name        = "/${var.environment}/tars/all/PrintGovNotify"
+  name        = "/${var.environment}/tars/all/Queue-PrintGovNotify"
   description = "Queue name for letters to Gov Notify."
   type        = "SecureString"
   value       = aws_sqs_queue.printgovnotify.name
