fed009e4073f35380f770dec72348c4661aa1a42
Christopher Swanson <christopher.swanson@bjss.com>
2022-05-18 11:24:58 +0100

    ops-4339-tars-ibs-critical

--- components/apps/cloudwatch_unhealthy_host_alarm.tf
+++ components/apps/cloudwatch_unhealthy_host_alarm.tf
@@ -19,6 +19,27 @@ resource "aws_cloudwatch_metric_alarm" "ibs_unhealthyhost" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "ibs_unhealthyhost_critical" {
+  alarm_name          = "${local.csi}-ibs-unhealthy-host-critical"
+
+  count               = var.unhealthy_host_alarm_count
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  metric_name         = "UnHealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "3"
+  alarm_description   = "Critical Unhealthy Hosts Count in the TARS ibs ASG"
+  alarm_actions       = [data.terraform_remote_state.base.outputs.sns_alerts_arn]
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    TargetGroup = aws_alb_target_group.ibs-8080.arn_suffix
+    LoadBalancer = aws_alb.apps.arn_suffix
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "obs_unhealthyhost" {
   alarm_name          = "${local.csi}-obs-unhealthy-host"
 
