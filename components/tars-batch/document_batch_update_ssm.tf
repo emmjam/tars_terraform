2150d01260c6ca9599183643c273001a0740ef8d
Raman Kapoor <kapoorr@LPT5869.bjss.co.uk>
2022-05-19 15:06:52 +0100

    param DocumentBatchUpdate-MaxRecordsProcessed to string

--- components/tars-batch/document_batch_update_ssm.tf
+++ components/tars-batch/document_batch_update_ssm.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "dbu_maxrecordsprocessed" {
   name        = "/${var.environment}/tars/batch/DocumentBatchUpdate-MaxRecordsProcessed"
   description = "DocumentBatchUpdate - Maximum records processed in the Job Polling"
-  type        = "SecureString"
+  type        = "String"
   value       = var.dbu_maxrecordsprocessed
 
   tags = merge(
