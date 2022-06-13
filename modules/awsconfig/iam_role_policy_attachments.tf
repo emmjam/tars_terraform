0d19508204728c9e3a2a0beb83471fb62e8fabfb
Aamir Soni <aamir.soni@bjss.com>
2022-06-13 09:31:59 +0100

    replace AWSConfigRole with AWS_ConfigRole

--- modules/awsconfig/iam_role_policy_attachments.tf
+++ modules/awsconfig/iam_role_policy_attachments.tf
@@ -1,7 +1,7 @@
 # The AWS-Managed Service Role Policy for AWS Config
 resource "aws_iam_role_policy_attachment" "awsconfig" {
   role       = aws_iam_role.main.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
 
 # The role policy created by this module to permit
