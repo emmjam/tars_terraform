89e2b7b33ac1cc3083e80c334588060ddd0ed519
daviesi <ian.davies@bjss.com>
2022-07-26 16:22:30 +0100

    Added TARS-Batch

--- components/tars-batch/efs_fs.tf
+++ components/tars-batch/efs_fs.tf
@@ -7,3 +7,13 @@ resource "aws_efs_file_system" "batch" {
   )
 }
 
+resource "aws_efs_file_system" "tars-batch-enc" {
+  encrypted = true
+
+  tags = merge(
+    local.default_tags,
+    {
+      "Name" = "${local.csi}/tars-batch-enc"
+    },
+  )
+}
