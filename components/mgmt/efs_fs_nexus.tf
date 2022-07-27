6504601d6df6a79ca25ea2a564581f1a4818a170
daviesi <ian.davies@bjss.com>
2022-07-27 11:34:31 +0100

    nexus

--- components/mgmt/efs_fs_nexus.tf
+++ components/mgmt/efs_fs_nexus.tf
@@ -7,3 +7,13 @@ resource "aws_efs_file_system" "nexus" {
   )
 }
 
+resource "aws_efs_file_system" "nexus-enc" {
+  encrypted = true
+
+  tags = merge(
+    local.default_tags,
+    {
+      "Name" = "${local.csi}-nexus-enc"
+    },
+  )
+}
