697b226ffff284cd2e184f7bbcab7bd0632a1406
daviesi <ian.davies@bjss.com>
2022-07-29 16:29:15 +0100

    reinstated data_amis.tf

---
+++ components/tars-batch/data_amis.tf
@@ -0,0 +1,32 @@
+# Get the wildfly batch AMI ID
+data "aws_ami" "wildfly-batch" {
+  most_recent = "true"
+
+  owners = [
+    data.aws_caller_identity.current.account_id,
+    var.mgmt_aws_account_id,
+  ]
+
+  filter {
+    name = "name"
+
+    values = [
+      format(
+        "%s-%s-%s/%s",
+        var.project,
+        "amzn2",
+        "tars-batch",
+        var.tars_ami_id,
+      )
+    ]
+  }
+
+  filter {
+    name = "state"
+
+    values = [
+      "available",
+    ]
+  }
+}
+
