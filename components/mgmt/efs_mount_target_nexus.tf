d3021d6b4b0c0ac91a819684485e6e131da44fe2
daviesi <ian.davies@bjss.com>
2022-08-03 16:32:49 +0100

    Reverted Nexus

--- components/mgmt/efs_mount_target_nexus.tf
+++ components/mgmt/efs_mount_target_nexus.tf
@@ -1,6 +1,6 @@
-resource "aws_efs_mount_target" "nexus-enc" {
+resource "aws_efs_mount_target" "nexus" {
   count          = length(var.nexus_efs_subnets_cidrs)
-  file_system_id = aws_efs_file_system.nexus-enc.id
+  file_system_id = aws_efs_file_system.nexus.id
   subnet_id      = element(module.subnets_nexus_efs.subnet_ids, count.index)
 
   security_groups = [
