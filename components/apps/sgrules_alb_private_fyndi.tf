45789be7b847692c064175774d366161da77f88f
tribbickm <michael.tribbick@bjss.com>
2022-08-08 16:48:17 +0100

    fyndi_tars_sg

--- components/apps/sgrules_alb_private_fyndi.tf
+++ components/apps/sgrules_alb_private_fyndi.tf
@@ -17,3 +17,11 @@ resource "aws_security_group_rule" "private-jmeter_egress_jenkinsnode_1099_1101"
   source_security_group_id = module.fyndi-b.security_group_id
 }
 
+resource "aws_security_group_rule" "private-alb_ingress_tars_batch" {
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "8080"
+  to_port                  = "8080"
+  security_group_id        = aws_security_group.private-alb.id
+  source_security_group_id = data.terraform_remote_state.tars-batch.outputs.tars-batch-sg-id
+}
