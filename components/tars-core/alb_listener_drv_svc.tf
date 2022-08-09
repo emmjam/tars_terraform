32677a67f859189c060f2ed44a0ee81207cf00c6
daviesi <ian.davies@bjss.com>
2022-08-09 14:55:11 +0100

    Added index

--- components/tars-core/alb_listener_drv_svc.tf
+++ components/tars-core/alb_listener_drv_svc.tf
@@ -1,12 +1,12 @@
 # TARS Public DVSA ALB Listener for port 443
 resource "aws_alb_listener" "tars_drv_svc_443" {
   count             = var.drv_svc_enabled == true ? 1 : 0
-  
+
   load_balancer_arn = aws_alb.tars_alb_drv_svc.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
-  certificate_arn   = data.aws_acm_certificate.driver_services_cert.arn
+  certificate_arn   = data.aws_acm_certificate.driver_services_cert[1].arn
 
   default_action {
     type        = "redirect"
--- components/tars-core/route53_records_public.tf
+++ components/tars-core/route53_records_public.tf
@@ -155,8 +155,8 @@ resource "aws_route53_record" "driver-services" {
   type    = "A"
 
   alias {
-    name                   = aws_alb.tars_alb_drv_svc.dns_name
-    zone_id                = aws_alb.tars_alb_drv_svc.zone_id
+    name                   = aws_alb.tars_alb_drv_svc[1].dns_name
+    zone_id                = aws_alb.tars_alb_drv_svc[1].zone_id
     evaluate_target_health = true
   }
 }
