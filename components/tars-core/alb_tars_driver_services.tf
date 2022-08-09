175b81f6d9a1c9b8e9245e29edec8c420829c9ef
daviesi <ian.davies@bjss.com>
2022-08-09 14:50:17 +0100

    Added index

--- components/tars-core/alb_tars_driver_services.tf
+++ components/tars-core/alb_tars_driver_services.tf
@@ -1,13 +1,13 @@
 resource "aws_alb" "tars_alb_drv_svc" {
   count            = var.drv_svc_enabled == true ? 1 : 0
-  
+
   name = format("%s-%s", local.csi, "drv-svc")
 
   internal     = "false"
   idle_timeout = 300
 
   security_groups = [
-    aws_security_group.tars_alb_drv_svc.id,
+    aws_security_group.tars_alb_drv_svc.id[0],
   ]
 
   subnets = data.terraform_remote_state.base.outputs.subnets_alb_public
