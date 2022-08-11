bc7fd8f4e66a97959590607d9562bb638c75f6a6
daviesi <ian.davies@bjss.com>
2022-08-11 14:46:03 +0100

    moved lambda to ctrl component

--- components/ctrl/lambda_ses_key_rotate.tf
+++ components/ctrl/lambda_ses_key_rotate.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "iam_ses_lambda" {
-  name = "iam_ses_lambda"
+  count = var.account_environment != "mgmt" ? 1 : 0
+  name  = "iam_ses_lambda"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -8,14 +9,14 @@ resource "aws_iam_role" "iam_ses_lambda" {
     Statement = [
       {
         Action = [
-            "iam:ListUsers",
-            "iam:CreateAccessKey",
-            "iam:DeleteAccessKey",
-            "iam:GetAccessKeyLastUsed",
-            "iam:GetUser",
-            "iam:ListAccessKeys",
-            "iam:UpdateAccessKey" 
-            ]
+          "iam:ListUsers",
+          "iam:CreateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:GetAccessKeyLastUsed",
+          "iam:GetUser",
+          "iam:ListAccessKeys",
+          "iam:UpdateAccessKey"
+        ]
         Effect   = "Allow"
         Sid      = ""
         Resource = "*"
@@ -30,6 +31,7 @@ resource "aws_iam_role" "iam_ses_lambda" {
 # --------------------------------------------------------------------------- #
 
 resource "aws_cloudwatch_log_group" "ses_key_rotate" {
+  count             = var.account_environment != "mgmt" ? 1 : 0
   name              = "/aws/lambda/${aws_lambda_function.ses_key_rotate.function_name}"
   retention_in_days = "90"
 
@@ -46,6 +48,7 @@ resource "aws_cloudwatch_log_group" "ses_key_rotate" {
 # --------------------------------------------------------------------------- #
 
 resource "aws_lambda_function" "ses_key_rotate" {
+  count            = var.account_environment != "mgmt" ? 1 : 0
   filename         = "${path.module}/files/ses_keys_rotate.zip"
   source_code_hash = filebase64sha256("${path.module}/files/ses_keys_rotate.zip")
 
@@ -69,6 +72,7 @@ resource "aws_lambda_function" "ses_key_rotate" {
 # --------------------------------------------------------------------------- #
 
 resource "aws_cloudwatch_event_rule" "ses_key_rotate_trigger" {
+  count       = var.account_environment != "mgmt" ? 1 : 0
   name        = "ses_key_rotate"
   description = "ses_key_rotate"
   # Run on the 1st of every 3rd month (roughly every 90 days)
@@ -76,11 +80,13 @@ resource "aws_cloudwatch_event_rule" "ses_key_rotate_trigger" {
 }
 
 resource "aws_cloudwatch_event_target" "ses_key_rotate" {
-  rule = aws_cloudwatch_event_rule.ses_key_rotate_trigger.name
-  arn  = aws_lambda_function.ses_key_rotate.arn
+  count = var.account_environment != "mgmt" ? 1 : 0
+  rule  = aws_cloudwatch_event_rule.ses_key_rotate_trigger.name
+  arn   = aws_lambda_function.ses_key_rotate.arn
 }
 
 resource "aws_lambda_permission" "allow_ses_rotate_cloudwatch" {
+  count         = var.account_environment != "mgmt" ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ses_key_rotate.function_name
--- components/mgmt/cloudwatch_matric_alarms_nexus.tf
+++ components/mgmt/cloudwatch_matric_alarms_nexus.tf
@@ -3,13 +3,13 @@
 # A cloudwatch metric will also need to be added for ebs utilisation for the nexus instance. This will be done as part of OPS-4468
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_nexus" {
-  alarm_name = "${format(
+  alarm_name = (format(
     "%s-%s-%s-%s",
     var.project,
     var.environment,
     var.component,
     "nexus-cpu-utilization"
-  )}"
+  ))
 
   alarm_description   = "This alarms if the CPU of the Nexus EC2 instance is exceeding 80%"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -24,18 +24,18 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_nexus" {
     AutoScalingGroupName = "${module.microservice_nexus.autoscaling_group_name}"
   }
 
-  treat_missing_data  = "notBreaching"
-  alarm_actions       = [ "${aws_sns_topic.nexus_opsgenie[0].arn}" ]
+  treat_missing_data = "notBreaching"
+  alarm_actions      = ["${aws_sns_topic.nexus_opsgenie[0].arn}"]
 }
 
 resource "aws_cloudwatch_metric_alarm" "nexus_alb_https_unhealthy_host_alert" {
-  alarm_name = "${format(
+  alarm_name = (format(
     "%s-%s-%s-%s",
     var.project,
     var.environment,
     var.component,
     "nexus-alb-https-unhealthy-host-alert"
-  )}"
+  ))
 
   alarm_description   = "nexus alb unhealthy host alert"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -51,6 +51,6 @@ resource "aws_cloudwatch_metric_alarm" "nexus_alb_https_unhealthy_host_alert" {
     "LoadBalancer" = "${aws_alb.public.arn_suffix}"
   }
 
-  treat_missing_data  = "notBreaching"
-  alarm_actions       = [ "${aws_sns_topic.nexus_opsgenie[0].arn}" ]
+  treat_missing_data = "notBreaching"
+  alarm_actions      = ["${aws_sns_topic.nexus_opsgenie[0].arn}"]
 }
