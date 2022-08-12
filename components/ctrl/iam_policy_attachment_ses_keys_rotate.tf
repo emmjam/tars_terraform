315db71c7aee8efb5591b95b24582a3031cb9ff9
daviesi <ian.davies@bjss.com>
2022-08-12 16:55:06 +0100

    fixes

--- components/ctrl/iam_policy_attachment_ses_keys_rotate.tf
+++ components/ctrl/iam_policy_attachment_ses_keys_rotate.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "ses_keys_rotate" {
   count      = var.account_environment != "mgmt" ? 1 : 0
-  policy_arn = aws_iam_policy.ses_keys_rotate.arn
+  policy_arn = aws_iam_policy.ses_keys_rotate[count.index].arn
   role = aws_iam_role.ses_keys_rotate.arn
 }
