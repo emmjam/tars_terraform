1ff1c6fc70b069bf5b2503896e75ae89ee3aac8b
daviesi <ian.davies@bjss.com>
2022-08-15 11:16:17 +0100

    Added logging

--- components/ctrl/lambda_ses_key_rotate.tf
+++ components/ctrl/lambda_ses_key_rotate.tf
@@ -43,9 +43,10 @@ resource "aws_lambda_permission" "allow_ses_rotate_cloudwatch" {
 
 
 resource "aws_cloudwatch_log_group" "ses_keys_rotate" {
-  count         = var.account_environment != "mgmt" ? 1 : 0
+  count             = var.account_environment != "mgmt" ? 1 : 0
   name              = "/aws/lambda/${aws_lambda_function.ses_keys_rotate[count.index].function_name}"
-
+  retention_in_days = 180
+  
   tags = merge(
     var.default_tags,
     {
