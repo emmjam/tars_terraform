90d574e63ec8b50a4a7f458bea94fdf477825a16
daviesi <ian.davies@bjss.com>
2022-09-13 15:17:10 +0100

    Deleting old resources

--- modules/jenkins/variables.tf
+++ modules/jenkins/variables.tf
@@ -142,10 +142,10 @@ variable "elb_subnets_cidrs" {
   description = "A list of CIDR blocks used for ELB subnets creation"
 }
 
-variable "nlb_subnets_cidrs" {
-  type        = list(string)
-  description = "A list of CIDR blocks used for NLB subnets creation"
-}
+#variable "nlb_subnets_cidrs" {
+#  type        = list(string)
+#  description = "A list of CIDR blocks used for NLB subnets creation"
+#}
 
 variable "elb_idle_timeout" {
   type        = string
