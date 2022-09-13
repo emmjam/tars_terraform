0a3a363fe020838bba7c0f2e93b8b7e254e132d4
daviesi <ian.davies@bjss.com>
2022-09-13 13:49:05 +0100

    Switch to NLB

--- modules/jenkins/variables.tf
+++ modules/jenkins/variables.tf
@@ -144,7 +144,7 @@ variable "elb_subnets_cidrs" {
 
 variable "nlb_subnets_cidrs" {
   type        = list(string)
-  description = "A list of CIDR blocks used for ALB subnets creation"
+  description = "A list of CIDR blocks used for NLB subnets creation"
 }
 
 variable "elb_idle_timeout" {
