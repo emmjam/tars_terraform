002c5dfb0a0972e3ced5c804219f1c136ca0b3af
daviesi <ian.davies@bjss.com>
2022-09-14 10:44:07 +0100

    Domain name to variable

--- components/tars-core/variables.tf
+++ components/tars-core/variables.tf
@@ -497,8 +497,3 @@ variable "drv_svc_enabled" {
   default     = false
   description = "Whether to create Driver Services resources. Required in Prod and UAT02"
 }
-
-variable "driver_services_domain" {
-  type        = string
-  description = "tars_alb_drv_svc ALB domain name"
-}
