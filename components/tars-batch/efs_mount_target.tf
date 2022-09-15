d7781499a6292ece03e173b0946b4f373baf4f25
daviesi <ian.davies@bjss.com>
2022-09-15 09:13:39 +0100

    merging

--- components/tars-batch/efs_mount_target.tf
+++ components/tars-batch/efs_mount_target.tf
@@ -1,10 +1,5 @@
 resource "aws_efs_mount_target" "tars-batch-enc" {
-<<<<<<< HEAD
   count          = length(var.batch_efs_subnets_cidrs)
-=======
-  #count          = length(var.batch_efs_subnets_cidrs)
-  count          = 3
->>>>>>> 6ffb161b56869323a4c00013a47ea019e93ecff1
   file_system_id = aws_efs_file_system.tars-batch-enc.id
   subnet_id      = element(module.subnets_batch_efs.subnet_ids, count.index)
 
