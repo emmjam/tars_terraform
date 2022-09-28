25a2f6892544b13bef5f344e8a12c74b25b0b410
daviesi <ian.davies@bjss.com>
2022-09-28 13:24:53 +0100

    rename nlb

--- modules/jenkins/nlb_listener.tf
+++ modules/jenkins/nlb_listener.tf
@@ -1,21 +1,21 @@
 resource "aws_lb_listener" "jenkins-8080" {
-  load_balancer_arn = aws_lb.jenkins.arn
+  load_balancer_arn = aws_lb.jenkins_nlb.arn
   port              = "8080"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins.arn
+    target_group_arn = aws_lb_target_group.jenkins_nlb.arn
   }
 }
 
 resource "aws_lb_listener" "jenkins-49187" {
-  load_balancer_arn = aws_lb.jenkins.arn
+  load_balancer_arn = aws_lb.jenkins_nlb.arn
   port              = "49187"
   protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins.arn
+    target_group_arn = aws_lb_target_group.jenkins_nlb.arn
   }
 }
