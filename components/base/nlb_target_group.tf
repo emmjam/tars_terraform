d20c0920670edb46b0a713ba658aee0f23d751f7
daviesi <ian.davies@bjss.com>
2022-10-04 09:52:40 +0100

    opsdev testing

---
+++ components/base/nlb_target_group.tf
@@ -0,0 +1,16 @@
+resource "aws_lb_target_group" "jenkins_nlb" {
+  name     = "${var.project}-${var.environment}-${var.component}-${var.name}"
+  port     = "8080"
+  protocol = "TCP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    protocol            = "HTTPS"
+    healthy_threshold   = var.elb_healthy_threshold
+    unhealthy_threshold = var.elb_unhealthy_threshold
+    timeout             = 10 #var.elb_healthcheck_timeout
+    interval            = 30 #var.elb_healthcheck_interval
+    path                = "/login"
+    matcher             = "200,399"
+  }
+}
