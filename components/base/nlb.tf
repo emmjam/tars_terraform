a7d74d0ce18570d700f2ee55112d3e99f023dc77
daviesi <ian.davies@bjss.com>
2022-10-04 09:55:29 +0100

    opsdev testing

--- components/base/nlb.tf
+++ components/base/nlb.tf
@@ -1,5 +1,16 @@
 resource "aws_lb" "jenkins_nlb" {
-  name                             = "${var.project}-${var.environment}-${var.component}-${var.name}-nlb"
+  xname        = "${var.module}-b"
+  project     = var.project
+  environment = var.environment
+  component   = var.component
+
+  vpc_id               = var.vpc_id
+  availability_zones   = slice(var.availability_zones, 0, length(var.availability_zones))
+  subnets_cidrs        = var.jenkins_blue_subnet_cidrs
+  subnets_route_tables = var.private_route_table_ids
+
+
+  name                             = "${var.project}-${var.environment}-${var.component}-${var.xname}-nlb"
   load_balancer_type               = "network"
   internal                         = var.elb_internal
   enable_cross_zone_load_balancing = var.elb_cross_zone_load_balancing
