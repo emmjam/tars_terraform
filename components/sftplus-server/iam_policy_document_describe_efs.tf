5b9e438f4ede19878fb9fec6f249cce6b259bfb6
daviesi <ian.davies@bjss.com>
2022-10-24 13:39:56 +0100

    renamed sftpplus_svr to sftpplus

--- components/sftplus-server/iam_policy_document_describe_efs.tf
+++ components/sftplus-server/iam_policy_document_describe_efs.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "sftpplus_svr_describe_efs" {
+data "aws_iam_policy_document" "sftpplus_describe_efs" {
   statement {
     sid    = "DescribeEFS"
     effect = "Allow"
@@ -13,13 +13,13 @@ data "aws_iam_policy_document" "sftpplus_svr_describe_efs" {
   }
 }
 
-resource "aws_iam_policy" "sftpplus_svr_describe_efs" {
-  name        = "${local.csi}-sftpplus_svr_describe_efs"
-  description = "IAM policy for ${local.csi}-sftpplus_svr_describe_efs"
-  policy      = data.aws_iam_policy_document.sftpplus_svr_describe_efs.json
+resource "aws_iam_policy" "sftpplus_describe_efs" {
+  name        = "${local.csi}-sftpplus_describe_efs"
+  description = "IAM policy for ${local.csi}-sftpplus_describe_efs"
+  policy      = data.aws_iam_policy_document.sftpplus_describe_efs.json
 }
 
-resource "aws_iam_role_policy_attachment" "sftpplus_svr_describe_efs" {
-  role       = module.sftpplus_svr.iam_role_name
-  policy_arn = aws_iam_policy.sftpplus_svr_describe_efs.arn
+resource "aws_iam_role_policy_attachment" "sftpplus_describe_efs" {
+  role       = module.sftpplus.iam_role_name
+  policy_arn = aws_iam_policy.sftpplus_describe_efs.arn
 }
