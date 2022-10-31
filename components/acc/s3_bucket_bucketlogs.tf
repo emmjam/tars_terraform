5da145bed1dd64fb15a40afc456dde8083a82bba
Raman Kapoor <kapoorr@LPT5869.bjss.co.uk>
2022-10-31 15:21:06 +0000

    TF 1.2 Upgrade - provider acc code fixes

--- components/acc/s3_bucket_bucketlogs.tf
+++ components/acc/s3_bucket_bucketlogs.tf
@@ -4,59 +4,7 @@
 # or any module called by this component
 resource "aws_s3_bucket" "bucketlogs" {
   bucket        = "${local.csi_global}-bucketlogs"
-  #acl           = "log-delivery-write"
   force_destroy = "true"
-
-  /*# Enable versioning
-  versioning {
-    enabled = true
-  }
-
-  # Enable Logging to Self
-  logging {
-    # Same as this bucket's name construction
-    # Wish terraform would fix ${self.id}
-    target_bucket = "${local.csi_global}-bucketlogs"
-
-    # Prefix with this bucket's name and a slash 
-    target_prefix = "${local.csi_global}-bucketlogs/"
-  }
-
-  # Rotate logs out to cheaper storage
-  # Delete after 2 years
-  lifecycle_rule {
-    id      = "wholebucket"
-    prefix  = "/"
-    enabled = "true"
-
-    transition {
-      days          = "30"
-      storage_class = "STANDARD_IA"
-    }
-
-    transition {
-      days          = "60"
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days = "732"
-    }
-
-    noncurrent_version_transition {
-      days          = "30"
-      storage_class = "STANDARD_IA"
-    }
-
-    noncurrent_version_transition {
-      days          = "60"
-      storage_class = "GLACIER"
-    }
-
-    noncurrent_version_expiration {
-      days = "732"
-    }
-  }*/
 
   tags = merge(
     local.default_tags,
--- components/acc/s3_bucket_cloudfront.tf
+++ components/acc/s3_bucket_cloudfront.tf
@@ -1,23 +1,6 @@
 resource "aws_s3_bucket" "driverpracticaltest-dvsa-gov-uk-apology" {
   count  = var.cloudfront_enabled
   bucket = "${local.csi_global}-apology-page"
-  /*website {
-    index_document = "index.html"
-  }
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = aws_s3_bucket.bucketlogs.id
-    target_prefix = "${local.csi_global}-apology-page/"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }*/
 }
 
 resource "aws_s3_bucket_website_configuration" "driverpracticaltest-dvsa-gov-uk-apology" {
