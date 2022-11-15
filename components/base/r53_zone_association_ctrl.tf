de5b10c2abae2e11c542e0831eadd7fed5e05883
tribbickm <michael.tribbick@bjss.com>
2022-11-15 11:23:55 +0000

    10157_vpc_association_change

--- components/base/r53_zone_association_ctrl.tf
+++ components/base/r53_zone_association_ctrl.tf
@@ -2,3 +2,8 @@ resource "aws_route53_zone_association" "vpc_ctrl" {
   zone_id = aws_route53_zone.vpc.zone_id
   vpc_id  = data.terraform_remote_state.ctrl.outputs.vpc_id
 }
+
+resource "aws_route53_zone_association" "vpc_ctrl_dvsacloud_uk" {
+  zone_id = aws_route53_zone.vpc_private.zone_id
+  vpc_id  = data.terraform_remote_state.ctrl.outputs.vpc_id
+}
