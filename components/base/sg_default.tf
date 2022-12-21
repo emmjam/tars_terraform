852be7541253c7c0800c74201830d6ac576946b7
Raman Kapoor <kapoorr@LPT5869.bjss.co.uk>
2022-12-21 14:26:54 +0000

    remove sg rules for default SGs

--- components/base/sg_default.tf
+++ components/base/sg_default.tf
@@ -2,18 +2,6 @@
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    protocol  = -1
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
-  }
-
   tags = merge(
     local.default_tags,
     {
