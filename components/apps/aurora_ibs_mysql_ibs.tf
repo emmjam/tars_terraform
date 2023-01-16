057ea98f99c98465989576e7ab85a02ae0f398b9
tribbickm <michael.tribbick@bjss.com>
2023-01-16 09:54:48 +0000

    ibsdb_10168_custom

--- components/apps/aurora_ibs_mysql_ibs.tf
+++ components/apps/aurora_ibs_mysql_ibs.tf
@@ -19,6 +19,7 @@ resource "aws_rds_cluster" "ibsdb_cluster" {
   iam_roles                       = [aws_iam_role.ibs_rds_role.arn]
   apply_immediately               = var.ibs_rds_apply_immediately
   engine_version                  = var.ibs_rds_engine_version
+  engine                          = var.ibs_aurora_engine
   allow_major_version_upgrade     = var.ibs_rds_major_version_upgrade
   vpc_security_group_ids = [
     aws_security_group.ibs_aurora.id,
