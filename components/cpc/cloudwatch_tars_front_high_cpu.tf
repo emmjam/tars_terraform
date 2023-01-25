e762f6075b387de623c2d99f78858addb75ec9d3
Richard Teasdale <richard.teasdale@bjss.com>
2023-01-25 08:36:03 +0000

    ops-4750 - Create TARS front CPU Alarm in Prep

--- components/cpc/cloudwatch_tars_front_high_cpu.tf
+++ components/cpc/cloudwatch_tars_front_high_cpu.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_metric_alarm" "tars_front_high_CPU" {
   alarm_name          = "${local.csi}-tars-front-high-cpu"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "5"
+  evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
   count               = var.tars_front_high_cpu_cw_metric_alarm_enabled
   namespace           = "AWS/EC2"
-  period              = "60"
-  datapoints_to_alarm = "5"
-  statistic           = "Maximum"
+  period              = "300"
+  datapoints_to_alarm = "1"
+  statistic           = "Average"
   threshold           = "60"
   alarm_description   = "This metric monitors TARS_Front ec2 cpu utilization"
   alarm_actions       = [data.terraform_remote_state.base.outputs.sns_alerts_arn]
