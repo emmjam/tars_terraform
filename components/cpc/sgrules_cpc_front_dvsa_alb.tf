094a1a69efa76d5b5478a462466797594282026c
Qayyum <ali.mohammed@bjss.com>
2023-02-10 09:47:02 +0000

    Added ingress rules on port 1000 for cidr block 10.0.0.1/32 on cpc alb

--- components/cpc/sgrules_cpc_front_dvsa_alb.tf
+++ components/cpc/sgrules_cpc_front_dvsa_alb.tf
@@ -10,6 +10,16 @@ resource "aws_security_group_rule" "cpc-front-alb_ingress_dvsa-443" {
   ]
 }
 
+#additional sg rule to allow ingress port 1000 for sftpplus  from 10.0.0.1/32
+resource "aws_security_group_rule" "cpc_ingress_rule" {
+  type        = "ingress"
+  from_port   = "1000"
+  to_port     = "1000"
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.1/32"]
+  security_group_id = aws_security_group.cpc-front-dvsa-alb.id
+}
+
 resource "aws_security_group_rule" "cpc-front-alb_egress_cpc-front-8443" {
   type                     = "egress"
   protocol                 = "tcp"
--- components/sftplus-server/sgrules_sftpplus_svr.tf
+++ components/sftplus-server/sgrules_sftpplus_svr.tf
@@ -10,6 +10,16 @@ resource "aws_security_group_rule" "sftpplus_ingress_cpc" {
   cidr_blocks = var.cpc-back_subnets_cidrs
 }
 
+#additional sg rule to allow ingress port 1000 for sftpplus  from 10.0.0.1/32
+resource "aws_security_group_rule" "sftpplus_ingress_rule" {
+  type        = "ingress"
+  from_port   = "1000"
+  to_port     = "1000"
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.1/32"]
+  security_group_id = module.sftpplus_svr.security_group_id
+}
+
 # Health checks from NLBs
 resource "aws_security_group_rule" "sftpplus_ingress_nlb" {
   description       = "Allow TCP/10022 from NLBs"
