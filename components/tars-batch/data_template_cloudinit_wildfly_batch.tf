3ae0724bc9163104af93eef6eb829d41644a0562
Raman Kapoor <kapoorr@LPT5869.bjss.co.uk>
2023-02-28 10:01:20 +0000

    retrieve d90 cert from parameter store for opsdev

--- components/tars-batch/data_template_cloudinit_wildfly_batch.tf
+++ components/tars-batch/data_template_cloudinit_wildfly_batch.tf
@@ -15,16 +15,17 @@ data "cloudinit_config" "wildfly-batch" {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/templates/wildfly_batch_setup.sh.tmpl",
       {
-        ENVIRONMENT      = var.environment
-        NODETYPE         = var.wildfly-batch_puppet_nodetype
-        AWS_ACCOUNT_ID   = var.aws_account_id
-        PRIVATE_DOMAIN   = var.private_domain_name
-        KMS_KEY          = data.terraform_remote_state.acc.outputs.hieradata_kms_key_id
-        EFS_ENC_ID       = aws_efs_file_system.tars-batch-enc.id
-        MOUNT_POINT_ENC  = "/efs"
-        DVSA_DNS_SERVERS = join(",", var.dvsa_dns_servers)
-        SEARCH_SUFFIX    = local.vpc_domain_name
-        LOG_GROUP        = local.tars_batch_log
+        ACCOUNT_ENVIRONMENT = var.account_environment
+        ENVIRONMENT         = var.environment
+        NODETYPE            = var.wildfly-batch_puppet_nodetype
+        AWS_ACCOUNT_ID      = var.aws_account_id
+        PRIVATE_DOMAIN      = var.private_domain_name
+        KMS_KEY             = data.terraform_remote_state.acc.outputs.hieradata_kms_key_id
+        EFS_ENC_ID          = aws_efs_file_system.tars-batch-enc.id
+        MOUNT_POINT_ENC     = "/efs"
+        DVSA_DNS_SERVERS    = join(",", var.dvsa_dns_servers)
+        SEARCH_SUFFIX       = local.vpc_domain_name
+        LOG_GROUP           = local.tars_batch_log
     })
   }
 }
--- components/tars-core/data_template_cloudinit_wildfly_back.tf
+++ components/tars-core/data_template_cloudinit_wildfly_back.tf
@@ -15,13 +15,14 @@ data "cloudinit_config" "wildfly-back" {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/templates/wildfly_back_setup.sh.tmpl",
       {
-        ENVIRONMENT    = var.environment
-        NODETYPE       = var.wildfly-back_puppet_nodetype
-        PRIVATE_DOMAIN = var.private_domain_name
-        KMS_KEY        = data.terraform_remote_state.acc.outputs.hieradata_kms_key_id
-        AWS_ACCOUNT_ID = var.aws_account_id
-        LOG_GROUP      = local.tars_back_log
-        OOM_BUCKET     = data.terraform_remote_state.acc.outputs.oom-s3-log-bucket_id
+        ACCOUNT_ENVIRONMENT = var.account_environment
+        ENVIRONMENT         = var.environment
+        NODETYPE            = var.wildfly-back_puppet_nodetype
+        PRIVATE_DOMAIN      = var.private_domain_name
+        KMS_KEY             = data.terraform_remote_state.acc.outputs.hieradata_kms_key_id
+        AWS_ACCOUNT_ID      = var.aws_account_id
+        LOG_GROUP           = local.tars_back_log
+        OOM_BUCKET          = data.terraform_remote_state.acc.outputs.oom-s3-log-bucket_id
     })
   }
 }
